ae1b2caacc351fcc4698e96276b6b11cfd1b0aa6
Jorge A. Espinoza <jorge.espinoza@grupo-sm.com>
2021-11-03 20:58:09 -0300

    Jorge

--- books/ciencias_sociales/5B/reader/src/pages/page006.jsx
+++ books/ciencias_sociales/5B/reader/src/pages/page006.jsx
@@ -40,11 +40,11 @@ function Page006() {
                     </div>
                         <div className="d-flex justify-content-center max-img mb-5">
                                 <div className="">
-                                    <img src="images/page_006/img_003.jpg" className="img-fluid" />
+                                    <img src={process.env.PUBLIC_URL + 'images/page_006/img_003.jpg'} className="img-fluid" />
                                 </div>
                             </div>
                             <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{bottom: 70, right:0}}>
-                                <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{transform: [{ rotate: '270deg'}], color: "white"}}>ABP &copy; SM</div>
+                                <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" >ABP &copy; SM</div>
                                 <div className="bgc-boros wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">06</div>
                             </div>
                         </div>
