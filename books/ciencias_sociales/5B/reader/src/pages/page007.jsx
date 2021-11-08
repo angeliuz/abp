c81c4db934fc990327e0b1e566a1b1cb4db6ad4d
Jorge A. Espinoza <jorge.espinoza@grupo-sm.com>
2021-11-08 17:21:23 -0300

    jorge

---
+++ books/ciencias_sociales/5B/reader/src/pages/page007.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import "./page_007.css";
+
+function Page007() {
+  return (
+    <div>
+      <div className="wrapper bgc-light">
+        <div className="d-flex flex-column align-items-center pbp-20">
+          <div className="page bgc-white overflow-hidden position-relative" id="page_07">
+            <div className="cabecera">
+              <div className="row ptp-20 px-4 m-0 centrar">
+                <div className="col-md-3 col-sm-3 p-0 m-0 color-garou f-IntroRustG-Base d-flex justify-content-center">
+                  <div className="caja wp-150 text-center fsp-24 ">SESIÓN 1</div>
+                </div>
+                <div className="col-md-9 col-sm-9 p-0 m-0 fsp-24 f-Ubuntu-M color-white cambio">
+                  <div className="section-name w-100 h-100 d-flex align-items-center">Tenemos un desafío</div>
+                </div>
+              </div>
+              <div className="row f-colby-compres-medium fsp-17 lh-sm text-white text-center psp-60 pep-60 justify-content-center hp-60">
+                <div className="col p-0 align-self-center">
+                  Mis objetivos en
+                  <br /> esta sesión son:
+                </div>
+                <div className="col-sm-auto p-0 align-self-end">
+                  <img src={process.env.PUBLIC_URL + "images/page_007/img_001.svg"} className="wp-40 rotar" alt="" />
+                </div>
+                <div className="col p-0 align-self-center">
+                  Averiguar en qué consiste
+                  <br /> el desafío.
+                </div>
+                <div className="col-sm-auto p-0 align-self-start">
+                  <img src={process.env.PUBLIC_URL + "images/page_007/img_003.svg"} className="wp-40 rotar" alt="" />
+                </div>
+                <div className="col p-0 align-self-center">
+                  Comprender la <br /> importancia del desafío.
+                </div>
+                <div className="col-sm-auto p-0 align-self-end">
+                  <img src={process.env.PUBLIC_URL + "images/page_007/img_001.svg"} className="wp-40 rotar" alt="" />
+                </div>
+                <div className="col-auto ptp-10 pbp-5 color-garou cb-caja">
+                  Comprometerme <br /> con el desafío.
+                </div>
+              </div>
+            </div>
+
+            <div className="ptp-10 psp-60 mbp-80 contenedor-pagina">
+              <div className="d-flex">
+                <div className="f-ccdigitaldelivery-bold fsp-35 color-goten">1</div>
+                <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10">Veo el video, leo la carta y destaco las ideas principales.</div>
+                <img src={process.env.PUBLIC_URL + "images/page_007/img_002.jpg"} className="cambio hp-40 text-center mtp-10 psp-10 pep-40" alt="" />
+              </div>
+              <div className="cb-paper">
+                <div className="cb-paper-content f-Ubuntu-L text-start">
+                  <img src={process.env.PUBLIC_URL + "images/page_007/img_002.png"} className="cb-carta" alt="" />
+                  <img src={process.env.PUBLIC_URL + "images/page_007/img_001.png"} className="cb-lapiz" alt="" />
+                  <p>Queridos estudiantes,</p>
+                  <p>Soy dirigente de una organización que promueve el respeto de los derechos de todas las personas en Chile.</p>
+
+                  <p>Junto con nuestro equipo, hemos impulsado numerosas iniciativas y reflexiones. A partir de ellas, aprendimos que es fundamental que todos los integrantes de la comunidad estén informados sobre los derechos y su situación actual.</p>
+
+                  <p>
+                    Es por ello que decidimos proponer un desafío a estudiantes de todo el país. Los invitamos a crear y publicar una revista centrada en los derechos de las personas. En ella incluirán noticias y un reportaje sobre la actualidad, entrevistas a personas de su entorno y una campaña de
+                    difusión. Además, plantearán su postura sobre el tema.
+                  </p>
+
+                  <p>El formato de la revista será digital e impreso, y es fundamental que la difundan en su comunidad escolar. De este modo, contribuirán a una sociedad más justa, consciente de sus logros y atenta a posibles mejoras.</p>
+
+                  <p>¡Quedamos atentos a su respuesta!</p>
+                  <p className="cb-paper-sign">
+                    Se despide con cariño, <br /> Nibaldo.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{ bottom: 70, right: 0 }}>
+              <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{ transform: `rotate(270deg)`, color: "dark" }}>
+                ABP &copy; SM
+              </div>
+              <div className="bgc-boros wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">7</div>
+            </div>
+            <div className="row w-100 position-absolute caja-flotante2 p-0 m-0 align-items-end" style={{ bottom: 0 }}>
+              <div className="col-7 col-sm-2 p-0 m-0 position-relative">
+                <img src={process.env.PUBLIC_URL + "images/rotulo_motivacion.svg"} className="visible rotulo-etapa position-absolute top-0 start-0" alt="" />
+                <div className="w-100 hp-15 bgc-yamcha rounded-caja-start"></div>
+                <div className="w-100 hp-25 bgc-goten"></div>
+              </div>
+              <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                <img src={process.env.PUBLIC_URL + "images/rotulo_planificacion.svg"} className="oculto" alt="" />
+                <div className="w-100 hp-15 bgc-tshinhan rounded-caja"></div>
+                <div className="w-100 hp-25 bgc-bulma"></div>
+              </div>
+              <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                <img src={process.env.PUBLIC_URL + "images/rotulo_investigacion.svg"} className="oculto" alt="" />
+                <div className="w-100 hp-15 bgc-milk rounded-caja"></div>
+                <div className="w-100 hp-25 bgc-android18"></div>
+              </div>
+              <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                <img src={process.env.PUBLIC_URL + "images/rotulo_experimentacion.svg"} className="oculto" alt="" />
+                <div className="w-100 hp-15 bgc-freezer rounded-caja"></div>
+                <div className="w-100 hp-25 bgc-pidgey"></div>
+              </div>
+              <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                <img src={process.env.PUBLIC_URL + "images/rotulo_creacion.svg"} className="oculto" alt="" />
+                <div className="w-100 hp-15 bgc-ash rounded-caja"></div>
+                <div className="w-100 hp-25 bgc-saitama"></div>
+              </div>
+              <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                <img src={process.env.PUBLIC_URL + "images/rotulo_difusion.svg"} className="oculto" alt="" />
+                <div className="w-100 hp-15 bgc-leono rounded-caja-end"></div>
+                <div className="w-100 hp-25 bgc-pikachu"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page007;
