b5b7dcd786dd46d2eaed10d39328db1856c712ff
ERIK PEREDA <erik.pereda@grupo-sm.com>
2022-01-11 16:47:29 -0300

    er

--- books/ciencias_sociales/3B/reader/src/App.js
+++ books/ciencias_sociales/3B/reader/src/App.js
@@ -39,6 +39,20 @@ import Page035 from "./pages/page035";
 import Page036 from "./pages/page036";
 import Page037 from "./pages/page037";
 import Page038 from "./pages/page038";
+import Page039 from "./pages/page039";
+import Page040 from "./pages/page040";
+import Page041 from "./pages/page041";
+import Page042 from "./pages/page042";
+import Page043 from "./pages/page043";
+import Page044 from "./pages/page044";
+import Page045 from "./pages/page045";
+import Page046 from "./pages/page046";
+import Page047 from "./pages/page047";
+import Page048 from "./pages/page048";
+import Page049 from "./pages/page049";
+import Page050 from "./pages/page050";
+import Page051 from "./pages/page051";
+import Page052 from "./pages/page052";
 
 
 function App() {
@@ -111,6 +125,19 @@ function App() {
       <Page036 numeroPagina={clearZeros} />
       <Page037 numeroPagina={clearZeros} />
       <Page038 numeroPagina={clearZeros} />
+      <Page039 numeroPagina={clearZeros} />
+      <Page040 numeroPagina={clearZeros} />
+      <Page042 numeroPagina={clearZeros} />
+      <Page043 numeroPagina={clearZeros} />
+      <Page044 numeroPagina={clearZeros} />
+      <Page045 numeroPagina={clearZeros} />
+      <Page046 numeroPagina={clearZeros} />
+      <Page047 numeroPagina={clearZeros} />
+      <Page048 numeroPagina={clearZeros} />
+      <Page049 numeroPagina={clearZeros} />
+      <Page050 numeroPagina={clearZeros} />
+      <Page051 numeroPagina={clearZeros} />
+      <Page052 numeroPagina={clearZeros} />
     </div>
   );
 }
