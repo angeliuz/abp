06114ba74394cee6a96baff3dd0ede7130bfea00
Jorge Espinoza <jorge.espinoza@grupo-sm.com>
2022-01-12 18:26:45 -0300

    Jorge soc 5

--- books/ciencias_sociales/5B/reader/src/pages/page044.jsx
+++ books/ciencias_sociales/5B/reader/src/pages/page044.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import InputBox from "../components/InputBox";
 import ModalVideo from "../components/ModalVideo";
 
-import "./page_043.css";
+import "./page_044.css";
 
-function Page043(props) {
+function Page044(props) {
 
-  const pagina = "043";
+  const pagina = "044";
   var indexInput = 0;
 
   return (
 
     <div className="wrapper bgc-light">
         <div className="d-flex flex-column align-items-center pbp-20">
-            <div className="cb-page43 page bgc-white overflow-hidden position-relative" id={"page_"+pagina}>
-                <img src="images/page_043/cabecera.svg" className="cb-dossier" alt=""/>
-
+            <div className="cb-page44 page bgc-white overflow-hidden position-relative" id={"page_"+pagina}>
+                <div className="cabecera-v1 hp-80 bgc-android18">
+                </div>
                 <div className="position-relative w-100 mbp-40">
                     <div className=" psp-80 pep-70 ptp-30 pbp-0 ">
                         <div className="fsp-16 f-Ubuntu-B text-start psp-0 pbp-10"> Esta página ha sido resuelta por</div>
                         <div className="psp-0 ptp-5 pbp-5 bgc-white rounded-p-10 border border-color-lightDark d-flex justify-content-center position-relative">
-                        <div className="position-absolute top-50 start-0 translate-middle wp-100 msp-0 mtp-0"><img src="images/page_043/img_002.png" className="w-100" /></div>
-                        <div className="position-absolute top-50 start-100 translate-middle wp-100 msp-0 mtp-0"><img src="images/page_043/img_008.png" className="w-50" /></div>
+                        <div className="position-absolute top-50 start-0 translate-middle wp-100 msp-0 mtp-0"><img src="images/page_044/img_002.png" className="w-100" /></div>
+                        <div className="position-absolute top-50 start-100 translate-middle wp-100 msp-0 mtp-0"><img src="images/page_044/img_008.png" className="w-50" /></div>
                             <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) }  className="px-1 text-start lhp-35 w-80" />
                         </div>
 
@@ -36,8 +36,8 @@ function Page043(props) {
                             Pensamos que lo que hemos aprendido nos va a servir en nuestra ruta para...
                         </div>
                         <div className="col-md-12 col-lg-12 position-relative">
-                           <div className="position-absolute top-0 start-100 translate-middle msp-0 mtp--10 visible-768"><img src="images/page_043/img_003.png" className="wp-100 " alt="" /></div> 
-                           <div className="position-absolute top-0 start-0 translate-middle msp-0 mtp-70 visible-768"><img src="images/page_043/img_004.png" className="wp-30 " alt="" /></div> 
+                           <div className="position-absolute top-0 start-100 translate-middle msp-0 mtp--10 visible-768"><img src="images/page_044/img_003.png" className="wp-100 " alt="" /></div> 
+                           <div className="position-absolute top-0 start-0 translate-middle msp-0 mtp-70 visible-768"><img src="images/page_044/img_004.png" className="wp-30 " alt="" /></div> 
                         
                             <div className="d-flex flex-column mbp-20 mtp-10 p-2 w-100 border-style-solid border-2 rounded-p-10 border-color-dark bgc-white">
                                 <div className="p-2 w-100 d-flex">
@@ -62,9 +62,9 @@ function Page043(props) {
                             Pensamos que los siguientes conceptos son claves para los siguientes pasos del proyecto.
                         </div>
                         <div className="col-md-12 col-lg-12 position-relative">
-                            <div className="position-absolute top-0 start-100 translate-middle msp-0 mtp-20 visible-768"><img src="images/page_043/img_005.png" className="wp-70 " alt="" /></div> 
-                            <div className="position-absolute top-100 start-0 translate-middle msp--5 mtp--90 visible-768"><img src="images/page_043/img_006.png" className="wp-40 " alt="" /></div> 
-                            <div className="position-absolute top-100 start-50 translate-middle msp-75 mtp-40 visible-768"><img src="images/page_043/img_007.png" className="wp-150 " alt="" /></div> 
+                            <div className="position-absolute top-0 start-100 translate-middle msp-0 mtp-20 visible-768"><img src="images/page_044/img_005.png" className="wp-70 " alt="" /></div> 
+                            <div className="position-absolute top-100 start-0 translate-middle msp--5 mtp--90 visible-768"><img src="images/page_044/img_006.png" className="wp-40 " alt="" /></div> 
+                            <div className="position-absolute top-100 start-50 translate-middle msp-75 mtp-40 visible-768"><img src="images/page_044/img_007.png" className="wp-150 " alt="" /></div> 
                         
                             <div className="d-flex flex-column mbp-20 mtp-10 p-2 w-100 border-style-solid border-2 rounded-p-10 border-color-dark bgc-white">
                                 <div className="p-2 w-100 d-flex">
@@ -107,4 +107,4 @@ function Page043(props) {
   );
 }
 
-export default Page043;
+export default Page044;
