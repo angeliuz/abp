b9886c9a948fa36b023cf2324df1001aa01155b4
clau <claudia.barba@grupo-sm.com>
2022-01-14 00:46:28 -0300

    clau

--- books/ciencias_naturales/4B/reader/src/pages/page076.jsx
+++ books/ciencias_naturales/4B/reader/src/pages/page076.jsx
@@ -15,8 +15,9 @@ function Page076(props) {
             <div className="d-flex flex-column align-items-center pbp-20">
               <div className="page cb-page76 bgc-white overflow-hidden position-relative" id={"page_"+pagina}>
                 {/* contenido DE PAGINA */}
-                <div className="cb-trescontenedores-p76 mtp-20 mbp-30">
-                    <p className="cb-texto-final-p76 f-Ubuntu-L fsp-13 mbp-10 text-start">En general, creemos que hemos trabajado en equipo…</p>
+                <div className="cb-contenedor-ejercicio-1-p76 pep-10 psp-10 ptp-5 pbp-30 mtp-80">
+                <div className="cb-border-b-p76 mtp-20 mbp-30">
+                    <p className="cb-texto-final-p76 f-Ubuntu-L fsp-15 mbp-10 text-start"> Ponernos de acuerdo ha sido:</p>
                     <div className="d-flex">
                     <div className="cb-lado-a-p76">
                         <div className="cb-izq-p76 w-100 d-flex">
@@ -57,11 +58,32 @@ function Page076(props) {
              </div>
          </div>
          {/* fin caritas */}
-            
-
-         <div className="text-center ptp-10 pbp-10 f-pixilate-regular fsp-15">porque:</div>
-                    <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="px-1 msp--10 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35" />
-                    
+         <div className="pep-10 psp-10">
+         <div className="text-start ptp-10 pbp-10 f-Ubuntu-L fsp-15">porque:</div>
+            <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="px-1 msp--10 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-70" />
+            <div className="text-start ptp-10 pbp-10 f-Ubuntu-L fsp-15">Para ponernos de acuerdo tuvimos que:</div>
+            <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="px-1 msp--10 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-70" />
+            </div>
+        </div>
+        {/* FIN EJERCICIO 1*/}
+        <div className="cb-ejercicio-final-p76 d-flex mtp-20">
+            <div className="cb-ejercicio-cajas-p76">
+                <div className="bgc-birdie pep-10 psp-10 ptp-10 cb-border-a-p76 mbp-10">
+                <div className="color-white f-Ubuntu-L fsp-15 fw-700 text-start"> Si volviésemos a empezar, volveríamos a...</div>
+                    <div className="bgc-white cb-border-5p-p76 psp-20 pep-15 pbp-10 ">
+                    <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="px-1 msp--10 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-170" />
+                    </div>
+                </div>
+                <div className="bgc-ken pep-10 psp-10 ptp-10 cb-border-a-p76">
+                <div className="color-white f-Ubuntu-L fsp-15 fw-700 text-start"> Si volviésemos a empezar, mejoraríamos en....</div>
+                    <div className="bgc-white cb-border-5p-p76 psp-20 pep-15 pbp-10 ">
+                    <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="px-1 msp--10 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-170" />
+                    </div>
+                </div>
+            </div>
+            <div className="cb-ejercicio-cajas-p76">personaje</div>
+        </div>
+        
               
                 
 
