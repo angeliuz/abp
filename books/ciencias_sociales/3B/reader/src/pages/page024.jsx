e835b5b3077546c1d1db59ea9f1cd1e9a58bd1e4
cristian1562 <cristian.acevedo@grupo-sm.com>
2022-01-14 18:31:54 -0300

    cristian

--- books/ciencias_sociales/3B/reader/src/pages/page024.jsx
+++ books/ciencias_sociales/3B/reader/src/pages/page024.jsx
@@ -5,77 +5,120 @@ import ModalBook from "../components/ModalBook";
 import "./page_024.css";
 
 function Page024(props) {
-
   const pagina = "024";
   var indexInput = 0;
 
-
-
   return (
-
     <div className="wrapper bgc-light">
-            <div className="d-flex flex-column align-items-center pbp-20">
-              <div className="page bgc-white overflow-hidden position-relative" id={"page_"+pagina}>
-            <div className="cabecera-v1 hp-80 bgc-android18">
-            </div>
-            <div className="ptp-10 container-titulo-global mbp-0">
+      <div className="d-flex flex-column align-items-center pbp-20">
+        <div
+          className="page bgc-white overflow-hidden position-relative"
+          id={"page_" + pagina}
+        >
+          <div className="cabecera-v1 hp-80 bgc-android18"></div>
+
+          <div className="row">
+            <div className="col-md-4 col-12">
+              <div className="ptp-10 container-titulo-global mbp-0">
                 <div className="d-flex">
-                    <img className="hp-20 align-center mt-3 me-2 mb-2" src="images/page_024/img_002.svg" alt=""  />
-                    <div className="f-ccdigitaldelivery-bold fsp-35 color-android18">2</div>
-                    <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">Me entrevisto en pareja. Luego, concluyo.</div>
+                  <img
+                    className="hp-20 align-center mt-3 me-2 mb-2"
+                    src="images/page_024/img_001.svg"
+                    alt=""
+                  />
+                  <div className="f-ccdigitaldelivery-bold fsp-35 color-android18">
+                    4
+                  </div>
+                  <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
+                    Construimos un afiche de la institución investigada
+                    considerando:
+                  </div>
                 </div>
-            </div>
-            
-
-           
-
-
-
-                {/* BEGIN PIE DE PAGINA */}
-                <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{ bottom: 70, right: 0 }}>
-                    <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{ transform: `rotate(270deg)`, color: "dark" }}>
-                        ABP &copy; SM
-                    </div>
-                    <div className="bgc-boros wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">{props.numeroPagina(pagina)}</div>
-                </div>
-                <div className="row w-100 position-absolute caja-flotante2 p-0 m-0 align-items-end" style={{ bottom: 0 }}>
-                    <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-                        <img src="images/rotulo_motivacion.svg" className="oculto" alt="" />
-                        <div className="w-100 hp-15 bgc-yamcha rounded-caja-start"></div>
-                        <div className="w-100 hp-25 bgc-goten"></div>
-                    </div>
-                    <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-                        <img src="images/rotulo_planificacion.svg" className="oculto" alt="" />
-                        <div className="w-100 hp-15 bgc-tshinhan rounded-caja"></div>
-                        <div className="w-100 hp-25 bgc-bulma"></div>
-                    </div>
-                    <div className="col-7 col-sm-2 p-0 m-0 position-relative">
-                        <img src="images/rotulo_investigacion.svg" className="visible rotulo-etapa position-absolute top-0 start-0" alt="" />
-                        <div className="w-100 hp-15 bgc-milk rounded-caja"></div>
-                        <div className="w-100 hp-25 bgc-android18"></div>
-                    </div>
-                    <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-                        <img src="images/rotulo_experimentacion.svg" className="oculto" alt="" />
-                        <div className="w-100 hp-15 bgc-freezer rounded-caja"></div>
-                        <div className="w-100 hp-25 bgc-pidgey"></div>
-                    </div>
-                    <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-                        <img src="images/rotulo_creacion.svg" className="oculto" alt="" />
-                        <div className="w-100 hp-15 bgc-ash rounded-caja"></div>
-                        <div className="w-100 hp-25 bgc-saitama"></div>
-                    </div>
-                    <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-                        <img src="images/rotulo_difusion.svg" className="oculto" alt="" />
-                        <div className="w-100 hp-15 bgc-leono rounded-caja-end"></div>
-                        <div className="w-100 hp-25 bgc-pikachu"></div>
-                    </div>
-                </div>
-                {/* END PIE DE PAGINA */}
-                
               </div>
+            </div>
+
+            <div className="col-md-8 position-relative">
+            <div className="position-absolute pin-24">
+              <img
+                src="images/page_024/img_001.png"
+                className=" "
+                alt=""
+              />
+            </div>
+              <div class=" margen-24 w-90 hp-500 p-3 text-left  border-style-solid  border-1 mep-20 msp-20 mtp-10 "></div>
             </div>
           </div>
 
+          {/* BEGIN PIE DE PAGINA */}
+          <div
+            className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2"
+            style={{ bottom: 70, right: 0 }}
+          >
+            <div
+              className="f-Ubuntu-M fsp-10 position-absolute mbp-60"
+              style={{ transform: `rotate(270deg)`, color: "dark" }}
+            >
+              ABP &copy; SM
+            </div>
+            <div className="bgc-boros wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">
+              {props.numeroPagina(pagina)}
+            </div>
+          </div>
+          <div
+            className="row w-100 position-absolute caja-flotante2 p-0 m-0 align-items-end"
+            style={{ bottom: 0 }}
+          >
+            <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+              <img
+                src="images/rotulo_motivacion.svg"
+                className="oculto"
+                alt=""
+              />
+              <div className="w-100 hp-15 bgc-yamcha rounded-caja-start"></div>
+              <div className="w-100 hp-25 bgc-goten"></div>
+            </div>
+            <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+              <img
+                src="images/rotulo_planificacion.svg"
+                className="oculto"
+                alt=""
+              />
+              <div className="w-100 hp-15 bgc-tshinhan rounded-caja"></div>
+              <div className="w-100 hp-25 bgc-bulma"></div>
+            </div>
+            <div className="col-7 col-sm-2 p-0 m-0 position-relative">
+              <img
+                src="images/rotulo_investigacion.svg"
+                className="visible rotulo-etapa position-absolute top-0 start-0"
+                alt=""
+              />
+              <div className="w-100 hp-15 bgc-milk rounded-caja"></div>
+              <div className="w-100 hp-25 bgc-android18"></div>
+            </div>
+            <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+              <img
+                src="images/rotulo_experimentacion.svg"
+                className="oculto"
+                alt=""
+              />
+              <div className="w-100 hp-15 bgc-freezer rounded-caja"></div>
+              <div className="w-100 hp-25 bgc-pidgey"></div>
+            </div>
+            <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+              <img src="images/rotulo_creacion.svg" className="oculto" alt="" />
+              <div className="w-100 hp-15 bgc-ash rounded-caja"></div>
+              <div className="w-100 hp-25 bgc-saitama"></div>
+            </div>
+            <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+              <img src="images/rotulo_difusion.svg" className="oculto" alt="" />
+              <div className="w-100 hp-15 bgc-leono rounded-caja-end"></div>
+              <div className="w-100 hp-25 bgc-pikachu"></div>
+            </div>
+          </div>
+          {/* END PIE DE PAGINA */}
+        </div>
+      </div>
+    </div>
   );
 }
 
