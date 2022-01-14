2eed493dbc35f2c692cdaa4f25f2bde67fad1146
ErikPereda <erik.pereda@gmail.com>
2022-01-14 12:47:43 -0300

    erik

--- books/ciencias_sociales/3B/reader/src/pages/page047.jsx
+++ books/ciencias_sociales/3B/reader/src/pages/page047.jsx
@@ -33,139 +33,82 @@ function Page047(props) {
               </div>
             </div>
  
-
-
-          <div className="d-flex msp-100 mep-60 position-relative mtp-30">
-            <div className="border border-3 p-3 hp-300 w-100 ">
-                <div><img src="images/page_043/img_004.png" className="position-absolute top-50 start-0 translate-middle wp-40" alt=""></img></div> 
-                <div className="f-Ubuntu-L text-start fsp-15 lh-sm msp-20 mbp-30">¿Qué elementos tiene la fiesta que se muestra en la revista?</div>
-                
-                <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) }  className="p-4 fsp-12 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-190" />
-            </div>
-            
-          </div>
+ 
 
 
 
+              <div className="d-flex justify-content-center msp-60 mep-40">
 
-          <div className="d-flex justify-content-center  msp-60 mep-40 mtp-40 ">
-              <div className="w-90 h-100 position-relative ">
-                
-                    <div className="position-absolute top-0 start-100 translate-middle img-parlante"><img src="images/page_045/img_005.png" className="hp-100 mtp-20 " alt=""/></div>
-                    
-                  <div className="row d-flex justify-content-center bgc-bobafett sombra-contenedor-45 ">
-
-                    <div className="col-lg-5 col-sm-12 m-0 p-0 hp-180 caja-sobre">
-                    <div className="position-absolute top-100 start-0 translate-middle"><img src="images/page_045/img_008.png" className="hp-80 mtp-100 " alt=""/></div>
-                    
-                      <div className="border-style-solid border-1 border-color-saitama mtp-10   bgc-white  hp-300 ">
-                        <div className="d-flex   ">
-                            <img className="hp-20 align-center mt-3 me-2 mb-2 msp--40" src="images/page_045/img_006.svg" alt="" />
-                            <div className="f-ccdigitaldelivery-bold fsp-35 color-saitama">2</div>
-                            <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-left">La música que queremos<br></br> incluir en nuestra fiesta será…</div>
-                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_033/circ.png" clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" /> */}
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-7 col-sm-12  m-0 bgc-bobafett  ptp-20 h-100 position-relative">
-
-                        <div className="position-absolute bottom-0 start-0 "><img src="images/page_045/img_009.png" className="hp-140 mtp--130 msp--210 img-monito-45" alt=""/></div>
-                      <div className="caja-debajo-45 mtp-10 position-relative border-style-solid border-1 border-color-saitama ">
-
-                          <div className=" bgc-white w-220  hp-385">
-                            <div className=" msp-100 pep-20 ptp-20 psp-0   text-start">
-
-                                <div className="d-flex bgc-white  w-100 ">
-                                    <img className="hp-20 align-center mt-3 me-2 mb-2  " src="images/page_045/img_007.svg" alt="" />
-                                    <div className="f-ccdigitaldelivery-bold fsp-35 color-saitama">3</div>
-                                    <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-left">Escribimos las ideas que queremos incluir en nuestra canción.</div>
-                                    {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_033/circ.png" clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" /> */}
-                                   
-                                </div>
-                                <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-230" />
-
-                                <div className="d-flex bgc-white  w-100 ">
-                                    {/* <img className="hp-20 align-center mt-3 me-2 mb-2  " src="images/page_045/img_007.svg" alt="" /> */}
-                                    <div className="f-ccdigitaldelivery-bold fsp-35 color-saitama">4</div>
-                                    <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-left">Escribimos nuestra canción y la guardamos para nuestra fiesta.</div>
-                                    {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_033/circ.png" clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" /> */}
-                                   
-                                </div>
+                <div className="position-relative"> <img className="hp-280 " src="images/page_047/img_002.png" alt="" /> 
+                    <div className="position-absolute top-50 start-50 translate-middle w-100">
+                        <div className="msp-40 mep-20">
+                            <div className=" linea1 min-hp-100  ">
+                                <div className="f-Ubuntu-L fsp-15 p-1 text-start">El texto que más me gustó fue…</div>
+                                <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-100" />
                             </div>
+                            <div className=" linea1 min-hp-100   ">
+                                <div className="f-Ubuntu-L fsp-15 p-1 text-start">Porque…</div>
+                                <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-100" />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+               </div>
+
+
+
+
+                <div className="d-flex justify-content-center msp-60 mep-40">
+                     <div className="row w-100 d-flex justify-content-center">
+
+                          <div className="col-6 d-flex h-100  ">
+                              <div className="d-flex">
+                                    {/* <img className="hp-15 align-center mt-3 me-2 mb-2" src="images/page_014/img_001.svg" alt="" /> */}
+                                    <div className="f-ccdigitaldelivery-bold fsp-35 color-saitama">2</div>
+                                    <div className="f-Ubuntu-R fsp-16 text-start ptp-18 psp-10">El texto narrativo que presentaré en nuestra feria de la diversidad será:…</div>
+                                    {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image="images/page_045/img_001.svg" clasesImagen="hp-40 text-center msp-30 mtp-10 " className="p-0 px-0" /> */}
+                              </div>  
                           </div>
 
-                      </div>
+                          <div className="col-md-6 d-flex justify-content-start  ">
+                                <div className="mtp-30 position-relative "><img className="hp-160" src="images/page_047/img_005.svg" alt="" />
+                                    <img className="hp-30 align-center  position-absolute top-0 start-100 translate-middle msp--6  mtp-20   " src="images/page_047/img_003.svg" alt="" />
+
+                                    <div className="position-absolute top-0 start-50 translate-middle-x  msp--40 mep-20 mtp-20 msp-20">
+                                        <div className="d-flex align-items-center pbp-10">
+                                            <div className="d-inline wp-30 hp-30 border-style-solid border-1 border-color-dark bgc-white"></div>
+                                            <div className="f-Ubuntu-L msp-10 fsp-15 lh-sm pep-5">Cuento.</div>
+                                        </div>
+                                        <div className="d-flex align-items-center pbp-10">
+                                            <div className="d-inline wp-30 hp-30 border-style-solid border-1 border-color-dark bgc-white"></div>
+                                            <div className="f-Ubuntu-L msp-10 fsp-15 lh-sm pep-5">Anécdota.</div>
+                                        </div>
+                                        <div className="d-flex align-items-center pbp-10">
+                                            <div className="d-inline wp-30 hp-30 border-style-solid border-1 border-color-dark bgc-white"></div>
+                                            <div className="f-Ubuntu-L msp-10 fsp-15 lh-sm pep-5">Narración.</div>
+                                        </div> 
+                                    </div>
+                                </div>
+
+
+
+                          </div>
+
                     </div>
-                  </div>
-
-
-
-              </div>
-          </div>
+                </div>
 
 
   
 
-
-
-            {/* <div className="ptp-10 psp-100 pep-100 mbp-0 contenedor-pagina position-relative">
-            <div className="position-absolute bottom-0 start-0"><img src="images/page_045/img_006.svg" className="wp-100 msp-120" alt=""/></div>
-            <div className="f-Ubuntu-B fsp-20 color-pidgey text-start lhp-20">Nos preparamos para elaborar nuestra encuesta</div>
-              <div className="row bgc-red">
-
-                <div className="col-md-4 col-12 psp-20 pep-20 pbp-20">
-                  <div className="d-flex bgc-yellow wp-300 caja-sobre">
-                    <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">2</div>
-                    <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-left">Elegimos cinco preguntas para elaborar nuestra encuesta con el material que nos entregarán.</div>
-                    <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_033/circ.png" clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" />
-                  </div>
+                <div className="d-flex msp-80 msp-40">
+                    {/* <img className="hp-20 align-center mt-3 me-2 mb-2" src="images/page_033/g_i.svg" alt="" /> */}
+                      <div className="f-ccdigitaldelivery-bold fsp-35 color-saitama">3</div>
+                      <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start">El texto que creará tendrá…</div>
+                      {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image="images/page_047/img_001.svg" clasesImagen="hp-40 msp-30 text-center mtp-10 " className="p-0 px-0" /> */}
+                      {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_045/img_001.svg" clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" /> */}
                 </div>
-
-                <div className="col-md-8 position-relative pbp-20 ptp-15">
-                  <div className=" w-100 p-3 text-left bgc-yoru border-style-solid border-color-goku border-1 ">
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿Qué edad tienes?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿De qué nacionalidad eres?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿Te consideras parte de o perteneces a un pueblo originario?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿Cuál es tu música favorita?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿Cuál es tu comida típica favorita?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿Cuál es tu deporte favorito?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿Qué te gusta hacer en tu tiempo libre?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">¿Cuál es tu asignatura favorita?</div>
-                    </div>
-                    <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_033/cuadro.svg" className="hp-30" alt="" /></div>
-                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Otra:</div>
-                      <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35" />
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div> */}
-
+ 
 
 
 
