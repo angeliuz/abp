fba8b739681f3288cd811df7879600a12e71448d
AldoJGonzalez <aldo.gonzalez@colex.grupo-sm.com>
2022-01-17 17:20:08 -0300

    Aldo 14Ene

--- books/ciencias_naturales/4B/reader/src/pages/page013.jsx
+++ books/ciencias_naturales/4B/reader/src/pages/page013.jsx
@@ -10,7 +10,7 @@ function Page013(props) {
   return (
     <div className="wrapper bgc-light">
     <div className="d-flex flex-column align-items-center pbp-10">
-      <div className="page position-relative bgc-motivacion" id={"page_"+pagina}>
+      <div className="page position-relative bgc-planificacion" id={"page_"+pagina}>
         <div className="mep-0 position-absolute top-0 end-0">
           <div className="d-flex justify-content-start bgc-goku color-goten ptp-55 pb-2 px-3 text-center mep-55 rounded-seb-20 lh-1">
             <div className="f-pixilate-regular fsp-25">
