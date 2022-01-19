b53c59a7eba0f2dafe73051c266406b92d72bf5c
davidjimenezsm <david.jimenez@grupo-sm.com>
2022-01-19 18:09:22 -0300

    david

--- books/ciencias_naturales/4B/reader/src/pages/page050.jsx
+++ books/ciencias_naturales/4B/reader/src/pages/page050.jsx
@@ -15,23 +15,105 @@ function Page050(props) {
                 {/* BEGIN CABECERA VERSIÓN 2 */}
                 <div className="cabecera-v2 bgc-pidgey d-flex flex-column">
                   <div className="d-flex contenedor-cabecera-v2 align-items-center ptp-40 psp-30 pep-30">
-                    <div className="caja-seccion wp-160 hp-60 d-flex justify-content-center ptp-10 mtp-5 color-pidgey fsp-24 f-IntroRustG-Base">SESIÓN 9</div>
-                    <div className="section-name d-flex align-items-center fsp-24 fw-700 f-Ubuntu-M color-white">¡Brigadas en el colegio!</div>
+                    <div className="caja-seccion wp-160 hp-60 d-flex justify-content-center ptp-10 mtp-5 color-pidgey fsp-24 f-IntroRustG-Base">SESIÓN 10</div>
+                    <div className="section-name d-flex align-items-center fsp-24 fw-700 f-Ubuntu-M color-white">Simulando una emergencia</div>
                   </div>
                 </div> 
                 {/* END CABECERA VERSIÓN 2 */}
 
-            <div className="ptp-10 psp-60 pep-60 mbp-0">
-              <div className="f-Ubuntu-B fsp-20 color-pidgey text-start lhp-20 psp-45">Las tareas de la brigada</div>
-              <div className="d-flex">
-              <img className="hp-20 align-center mt-3 me-2 mb-2" src="images/page_045/g_i.svg" alt="" />
-                <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">1</div>
-                <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start">Leemos la revista. Luego, nos organizamos como equipo para que cada integrante sea responsable de realizar y monitorear una tarea específica.</div>
-                <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_045/revista.svg" clasesImagen="cambio hp-35 mtp-20" className="p-0 px-1" />
+              <div className="ptp-10 psp-100 pep-60 mbp-0">
+                <div className="f-Ubuntu-B fsp-20 color-pidgey text-start lhp-20">Ensayamos la presentación</div>
+                <div className="d-flex">
+                  <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">1</div>
+                  <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start w-85">Con mi equipo, revisamos el guion y ensayamos el simulacro. Luego, marcamos con un <img src="images/page_050/ticket.svg" className="mbp-10" alt=""></img> lo que está listo.</div>
+                </div>
               </div>
-            </div>
 
-            <div className="ptp-10 psp-130 pep-80 mbp-0">
+              <div className="psp-130 pep-100 mbp-0">
+                <div className="col-12 position-relative pbp-20">
+                  <div className="position-absolute top-50 start-100 translate-middle"><img src="images/page_050/posit.png" className="hp-60 msp-30 mbp-40" alt=""/></div>
+                  <div className="bloque-respuesta w-100 psp-40 pep-40 ptp-10 pbp-10 bgc-yoru bgc-bang">
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="pep-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5 text-start">Cada integrante conoce y domina el guion.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="pep-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5 text-start">Preparamos todos los materiales o insumos necesarios.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="pep-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5 text-start">Contamos con la vestimenta adecuada.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="pep-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5 text-start">Disponemos de los recursos digitales que utilizaremos.</div>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <div className="pep-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm pep-5 text-start">Si necesitamos apoyo extra, ya lo tenemos previsto.</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="ptp-10 psp-100 pep-60 mbp-0">
+                <div className="f-Ubuntu-B fsp-20 color-pidgey text-start lhp-20">Acuerdos previos a la presentación</div>
+                <div className="d-flex">
+                  <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">2</div>
+                  <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start">Como curso, sorteamos el orden en que se realizarán las presentaciones de los simulacros.</div>
+                </div>
+              </div>
+
+              <div className="psp-110 pep-90 mbp-0 ptp-10">
+                <div className="col-12 position-relative pbp-20">
+                <div className="position-absolute top-50 start-0 translate-middle"><img src="images/page_050/lapiz.png" className="hp-80 mep-20 mtp-0" alt=""/></div>
+                  <div className="w-100 p-4 bgc-yoru bgc-honda rounded-p-10 border-style-solid border-color-goku border-3">
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="f-Ubuntu-M fsp-15 lh-sm text-center w-10">Turno</div>
+                      <div className="w-10 bgc-goku"></div>
+                      <div className="f-Ubuntu-M fsp-15 lh-sm text-start psp-10 w-80">Emergencia</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="w-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="w-10 hp-2 bgc-goku"></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm text-start rounded-p-5 psp-10 pep-10 ptp-5 pbp-5 bgc-white w-80">Ocurre un sismo mientras estamos en la sala de clases.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="w-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="w-10 hp-2 bgc-goku"></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm text-start rounded-p-5 psp-10 pep-10 ptp-5 pbp-5 bgc-white w-80">Un estudiante se cae al correr por las escaleras durante el recreo.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="w-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="w-10 hp-2 bgc-goku"></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm text-start rounded-p-5 psp-10 pep-10 ptp-5 pbp-5 bgc-white w-80">Se rompe un material de vidrio durante un experimento científico.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="w-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="w-10 hp-2 bgc-goku"></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm text-start rounded-p-5 psp-10 pep-10 ptp-5 pbp-5 bgc-white w-80">Hay una amago de incendio en la sala de profesores.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="w-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="w-10 hp-2 bgc-goku"></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm text-start rounded-p-5 psp-10 pep-10 ptp-5 pbp-5 bgc-white w-80">Se siente olor a gas en el casino del colegio.</div>
+                    </div>
+                    <div className="d-flex align-items-center pbp-10">
+                      <div className="w-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="w-10 hp-2 bgc-goku"></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm text-start rounded-p-5 psp-10 pep-10 ptp-5 pbp-5 bgc-white w-80">Un estudiante se siente enfermo durante la clase.</div>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <div className="w-10"><img src="images/page_050/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="w-10 hp-2 bgc-goku"></div>
+                      <div className="f-Ubuntu-L fsp-15 lh-sm text-start rounded-p-5 psp-10 pep-10 ptp-5 pbp-5 bgc-white w-80">Se derrama agua caliente en una actividad extraprogramática.</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            {/* <div className="ptp-10 psp-130 pep-80 mbp-0">
               <div className="row mbp-10 position-relative">
                 <div className="position-absolute top-0 start-0 translate-middle"><img src="images/page_045/bombero.png" className="wp-45 mep-50 mtp-150" alt=""/></div>
                 <div className="col-7 rounded-p-5 bgc-voldemort border-style-solid border-color-bulma border-2 text-center text-white f-Ubuntu-B fsp-15 p-1 mb-1">Tarea</div>
@@ -57,9 +139,10 @@ function Page050(props) {
                   <InputBox id={ "p" + pagina + "_input"+ (indexInput+=1) } className="p-0 px-1 text-start flex-grow-1 lhp-30 min-hp-35" />
                 </div>
               </div>
-            </div>
+            </div> */}
 
-            <div className="ptp-10 psp-100 pep-60 mbp-0">
+            {/* <div className="ptp-10 psp-100 pep-60 mbp-0">
+            <div className="f-Ubuntu-B fsp-20 color-pidgey text-start lhp-20">Ensayamos la presentación</div>
               <div className="d-flex">
                 <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">2</div>
                 <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start">Considerando la misión de la brigada, explico la importancia que tiene la tarea de la que seré responsable.</div>
@@ -97,7 +180,7 @@ function Page050(props) {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
 
             <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{ bottom: 70, right: 0 }}>
               <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{ transform: `rotate(270deg)`, color: "dark" }}>ABP &copy; SM</div>
