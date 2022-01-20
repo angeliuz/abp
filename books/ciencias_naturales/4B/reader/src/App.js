b0c9b1d5382547088b62d0ebea727fce2a7e135d
davidjimenezsm <david.jimenez@grupo-sm.com>
2022-01-20 15:17:22 -0300

    david

--- books/ciencias_naturales/4B/reader/src/App.js
+++ books/ciencias_naturales/4B/reader/src/App.js
@@ -50,6 +50,7 @@ import Page049 from "./pages/page049";
 import Page050 from "./pages/page050";
 import Page051 from "./pages/page051";
 import Page052 from "./pages/page052";
+import Page053 from "./pages/page053";
 import Page057 from "./pages/page057";
 import Page058 from "./pages/page058";
 import Page059 from "./pages/page059";
@@ -153,6 +154,7 @@ function App() {
       <Page050 numeroPagina={clearZeros} />
       <Page051 numeroPagina={clearZeros} />
       <Page052 numeroPagina={clearZeros} />
+      <Page053 numeroPagina={clearZeros} />
       <Page057 numeroPagina={clearZeros} />
       <Page058 numeroPagina={clearZeros} />
       <Page059 numeroPagina={clearZeros} />
