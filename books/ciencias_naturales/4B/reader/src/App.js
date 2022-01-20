a391e30bfafce3a2a091db03435c05391999cb33
cristian1562 <cristian.acevedo@grupo-sm.com>
2022-01-20 15:30:18 -0300

    cristian

--- books/ciencias_naturales/4B/reader/src/App.js
+++ books/ciencias_naturales/4B/reader/src/App.js
@@ -39,6 +39,7 @@ import Page035 from "./pages/page035";
 import Page036 from "./pages/page036";
 import Page037 from "./pages/page037";
 import Page038 from "./pages/page038";
+import Page041 from "./pages/page041";
 import Page042 from "./pages/page042";
 import Page043 from "./pages/page043";
 import Page044 from "./pages/page044";
@@ -144,6 +145,7 @@ function App() {
       <Page036 numeroPagina={clearZeros} />
       <Page037 numeroPagina={clearZeros} />
       <Page038 numeroPagina={clearZeros} />
+      <Page041 numeroPagina={clearZeros} />
       <Page042 numeroPagina={clearZeros} />
       <Page043 numeroPagina={clearZeros} />
       <Page044 numeroPagina={clearZeros} />
