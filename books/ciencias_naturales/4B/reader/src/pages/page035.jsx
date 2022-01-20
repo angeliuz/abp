f82808b83624b90b17c95bd1d7104241cde874a8
AldoJGonzalez <aldo.gonzalez@colex.grupo-sm.com>
2022-01-20 16:14:15 -0300

    Aldo 20Ene

--- books/ciencias_naturales/4B/reader/src/pages/page035.jsx
+++ books/ciencias_naturales/4B/reader/src/pages/page035.jsx
@@ -20,7 +20,7 @@ function Page035(props) {
           {/* END CABECERA VERSIÓN 2 */}
           <div className="ptp-30 psp-60 pep-60">
             <div className="f-Ubuntu-M fsp-20 color-android18 text-start">
-            Preparamos una encuesta
+              Preparamos una encuesta
             </div>
           </div>
           <div className="ptp-10 container-titulo-global mbp-20">
@@ -29,7 +29,7 @@ function Page035(props) {
                 2
               </div>
               <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
-              Leo la revista y completo.
+                Leo la revista y completo.
               </div>
             </div>
           </div>
@@ -40,8 +40,8 @@ function Page035(props) {
                 <img src="images/page_035/img_035.png" className=" " alt="" />
               </div>
               <div className="box-color-24-1">
-                <div className="f-Ubuntu-M alinea fsp-18 rounded-caja bgc-vegeta w-100 p-2 text-center color-white">
-                El propósito de una encuesta es:
+                <div className="f-Ubuntu-M alinea fsp-18 rounded-caja bgc-vegeta w-100 pbp-20 ptp-20 px-2 text-center color-white">
+                  El propósito de una encuesta es:
                 </div>
                 <div className=" linea1 bgc-white min-hp-200 p-2  rounded-seb-20 border-style-solid border-color-vegeta border-2">
                   <InputBox
@@ -52,20 +52,19 @@ function Page035(props) {
               </div>
             </div>
 
-            <div className="col-sm-12 col-md-6 position-relative mbp-40">
-              <div className="position-absolute top-0 start-100 translate-middle visible-768 zindex-2 msp--50 mtp--50">
-                <img src="images/page_031/img_001.png" className="" alt="" />
-              </div>
-
+            <div className="col-sm-12 col-md-6 mbp-40">
               <div className="box-color-24-2">
-                <div className="f-Ubuntu-M alinea fsp-18 rounded-caja bgc-saitama w-100 p-2 text-center color-white">
-                  Palabras que no entiendo
+                <div className="f-Ubuntu-M alinea fsp-18 rounded-caja bgc-saitama w-100 h-100 p-2 text-center color-white">
+                  Para el proyecto, sería útil realizar una encuesta sobre:
                 </div>
-                <div className="linea1 bgc-white min-hp-200 p-2  rounded-seb-20 border-style-solid border-color-saitama border-2">
+                <div className="linea1 bgc-white min-hp-200 p-2  rounded-seb-20 border-style-solid border-color-saitama border-2 position-relative">
                   <InputBox
                     id={"p" + pagina + "_input" + (indexInput += 1)}
                     className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-175"
                   />
+                  <div className="position-absolute top-50 start-0 translate-middle h-100 ptp-10 pbp-10 msp--15 visible-768">
+                    <div className="anillos-p35 wp-50 h-100"></div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -76,8 +75,206 @@ function Page035(props) {
                 3
               </div>
               <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
-                Busco el significado de las palabras que desconozco.
+                Con mi equipo, elegimos 4 preguntas para aplicar en nuestra
+                encuesta.
               </div>
+            </div>
+          </div>
+          <div className="psp-60 pep-60">
+            <div className="row">
+              <div className="col-md-6 col-12">
+                <div className="ptp-10 pbp-10 psp-20">
+                  <div className="d-flex">
+                    <div className="cuadrado-p35 bgc-white border-style-solid border-1 border-color-dark">
+                      <InputBox
+                        id={"p" + pagina + "_input" + (indexInput += 1)}
+                        className="p-0 px-1 text-start flex-grow-1 hp-25 min-hp-25"
+                      />
+                    </div>
+                    <div className="f-Ubuntu-L psp-15 ptp-5 text-start">
+                      ¿Está al tanto del plan de seguridad del colegio?
+                    </div>
+                  </div>
+                </div>
+                <div className="ptp-10 pbp-10 psp-20">
+                  <div className="d-flex">
+                    <div className="cuadrado-p35 bgc-white border-style-solid border-1 border-color-dark">
+                      <InputBox
+                        id={"p" + pagina + "_input" + (indexInput += 1)}
+                        className="p-0 px-1 text-start flex-grow-1 hp-25 min-hp-25"
+                      />
+                    </div>
+                    <div className="f-Ubuntu-L psp-15 ptp-5 text-start">
+                      ¿Conoce las vías de evacuación en el colegio?
+                    </div>
+                  </div>
+                </div>
+                <div className="ptp-10 pbp-10 psp-20">
+                  <div className="d-flex">
+                    <div className="cuadrado-p35 bgc-white border-style-solid border-1 border-color-dark">
+                      <InputBox
+                        id={"p" + pagina + "_input" + (indexInput += 1)}
+                        className="p-0 px-1 text-start flex-grow-1 hp-25 min-hp-25"
+                      />
+                    </div>
+                    <div className="f-Ubuntu-L psp-15 ptp-5 text-start">
+                      ¿Sabe dónde se ubican las zonas de seguridad en el
+                      colegio?
+                    </div>
+                  </div>
+                </div>
+                <div className="ptp-10 pbp-10 psp-20">
+                  <div className="d-flex">
+                    <div className="cuadrado-p35 bgc-white border-style-solid border-1 border-color-dark">
+                      <InputBox
+                        id={"p" + pagina + "_input" + (indexInput += 1)}
+                        className="p-0 px-1 text-start flex-grow-1 hp-25 min-hp-25"
+                      />
+                    </div>
+                    <div className="f-Ubuntu-L psp-15 ptp-5 text-start">
+                      ¿Sabe usar un extintor?
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-12">
+                <div className="ptp-10 pbp-10 psp-20">
+                  <div className="d-flex">
+                    <div className="cuadrado-p35 bgc-white border-style-solid border-1 border-color-dark">
+                      <InputBox
+                        id={"p" + pagina + "_input" + (indexInput += 1)}
+                        className="p-0 px-1 text-start flex-grow-1 hp-25 min-hp-25"
+                      />
+                    </div>
+                    <div className="f-Ubuntu-L psp-15 ptp-5 text-start">
+                      ¿Qué haría en caso de presenciar un accidente en el
+                      colegio?
+                    </div>
+                  </div>
+                </div>
+                <div className="ptp-10 pbp-10 psp-20">
+                  <div className="d-flex">
+                    <div className="cuadrado-p35 bgc-white border-style-solid border-1 border-color-dark">
+                      <InputBox
+                        id={"p" + pagina + "_input" + (indexInput += 1)}
+                        className="p-0 px-1 text-start flex-grow-1 hp-25 min-hp-25"
+                      />
+                    </div>
+                    <div className="f-Ubuntu-L psp-15 ptp-5 text-start">
+                      ¿Quién cree que es el encargado de la seguridad en el
+                      colegio?
+                    </div>
+                  </div>
+                </div>
+                <div className="ptp-10 pbp-10 psp-20">
+                  <div className="d-flex">
+                    <div className="cuadrado-p35 bgc-white border-style-solid border-1 border-color-dark">
+                      <InputBox
+                        id={"p" + pagina + "_input" + (indexInput += 1)}
+                        className="p-0 px-1 text-start flex-grow-1 hp-25 min-hp-25"
+                      />
+                    </div>
+                    <div className="f-Ubuntu-L psp-15 ptp-5 text-start">
+                      ¿A quién pediría ayuda en caso de una emergencia?
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="ptp-10 container-titulo-global mbp-20">
+            <div className="d-flex">
+              <div className="f-ccdigitaldelivery-bold fsp-35 color-android18">
+                4
+              </div>
+              <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
+                Planteamos otras preguntas para incorporar a la encuesta.
+              </div>
+            </div>
+          </div>
+          <div className="psp-90 pep-50">
+            <div className="mtp--20">
+              <InputBox
+                id={"p" + pagina + "_input" + (indexInput += 1)}
+                className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-120"
+              />
+            </div>
+          </div>
+          <div className="ptp-10 container-titulo-global mbp-20">
+            <div className="d-flex">
+              <div className="f-ccdigitaldelivery-bold fsp-35 color-android18">
+                5
+              </div>
+              <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
+                Ideamos un plan para aplicar la encuesta.
+              </div>
+            </div>
+          </div>
+          <div className="psp-50 pep-50">
+            <div className="bgc-white border-style-solid border-1 border-color-lightDark sombra w-100 psp-40 pep-15 ptp-10 pbp-130 position-relative">
+                <div className="ptp-10 text-start">Personas a quienes se encuestará:</div>
+                <div className="flex-grow-1 mtp--5">
+                  {" "}
+                  <InputBox
+                    id={"p" + pagina + "_input" + (indexInput += 1)}
+                    className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-75 w-100"
+                  />
+                </div>
+              <div className="row">
+                <div className="col-md-6 d-flex">
+                  <div className="ptp-10">Cantidad de personas:</div>
+                  <div className="flex-grow-1">
+                    {" "}
+                    <InputBox
+                      id={"p" + pagina + "_input" + (indexInput += 1)}
+                      className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35 w-100"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 d-flex">
+                  <div className="ptp-10">Fecha de la encuesta</div>
+                  <div className="flex-fill">
+                  {" "}
+                  <InputBox
+                    id={"p" + pagina + "_input" + (indexInput += 1)}
+                    className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35 w-100"
+                  />
+                </div>
+                <span className="ptp-10">/</span>
+                <div className="flex-fill">
+                  {" "}
+                  <InputBox
+                    id={"p" + pagina + "_input" + (indexInput += 1)}
+                    className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35 w-100"
+                  />
+                </div>
+                <span className="ptp-10">/</span>
+                <div className="flex-fill">
+                  {" "}
+                  <InputBox
+                    id={"p" + pagina + "_input" + (indexInput += 1)}
+                    className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35 w-100"
+                  />
+                </div>
+                </div>
+              </div>
+              <div className="d-flex">
+                <div className="ptp-10">Tiempo destinado para encuestar:</div>
+                <InputBox
+                    id={"p" + pagina + "_input" + (indexInput += 1)}
+                    className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35"
+                  />
+              </div>
+              <div className="d-flex">
+                <div className="ptp-10">Lugar de realización de la encuesta:</div>
+                <InputBox
+                    id={"p" + pagina + "_input" + (indexInput += 1)}
+                    className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35"
+                  />
+              </div>
+              <div className="position-absolute top-50 start-0 translate-middle h-100 ptp-10 pbp-10 visible-768">
+                    <div className="anillos-b-p35 wp-30 h-100"></div>
+                  </div>
             </div>
           </div>
           {/* BEGIN PIE DE PAGINA */}
