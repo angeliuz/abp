ec11889f18f62384f7f29131e8bde8488a3cafa1
davidjimenezsm <david.jimenez@grupo-sm.com>
2022-01-21 18:05:06 -0300

    david

--- books/ciencias_sociales/3B/reader/src/pages/page034.jsx
+++ books/ciencias_sociales/3B/reader/src/pages/page034.jsx
@@ -16,12 +16,11 @@ function Page034(props) {
                 <div className="cabecera-v1 hp-80 bgc-pidgey"></div> 
                 {/* END CABECERA VERSIÓN 2 */}
 
-            <div className="ptp-10 psp-100 pep-100 mbp-0 contenedor-pagina position-relative">
-            <div className="position-absolute top-50 start-0"><img src="images/page_034/agenda.png" className="wp-50 msp-180 mbp-90" alt=""/></div>
-            <div className="position-absolute top-0 end-0 zindex-2"><img src="images/page_034/clip.png" className="wp-30 mep-110 mtp-5" alt=""/></div>
+            <div className="ptp-20 psp-75 pep-60 position-relative">
+            <div className="position-absolute top-50 start-0 visible-768"><img src="images/page_034/agenda.png" className="wp-50 msp-180 mbp-90" alt=""/></div>
               <div className="row">
 
-                <div className="col-md-5 col-12 psp-20 pep-20 pbp-20">
+                <div className="col-md-5 col-12">
                   <div className="d-flex">
                     <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">3</div>
                     <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-left">Las personas a las que encuestaremos serán…</div>
@@ -29,21 +28,22 @@ function Page034(props) {
                 </div>
 
                 <div className="col-md-7 position-relative pbp-20 ptp-15 rota">
+                  <div className="position-absolute top-0 end-0 zindex-2"><img src="images/page_034/clip.png" className="wp-30 mep-40 mtp--5" alt=""/></div>
                   <div className="bloque-respuesta w-100 p-3 text-left bgc-draco rota">
                     <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                       <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Compañeros de curso.</div>
                     </div>
                     <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                       <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Profesores y profesoras.</div>
                     </div>
                     <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                       <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Compañeros de otros cursos.</div>
                     </div>
                     <div className="d-flex align-items-center pbp-10">
-                      <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                      <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                       <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Otros trabajadores del colegio.</div>
                     </div>
                   </div>
@@ -52,12 +52,12 @@ function Page034(props) {
               </div>
             </div>
 
-            <div className="ptp-10 psp-60 mbp-0 contenedor-pagina">
-              <div className="f-Ubuntu-B psp-50 fsp-20 color-pidgey text-start lhp-20">¡Nos ponemos en acción!</div>
+            <div className="ptp-10 psp-40 pep-40">
+              <div className="f-Ubuntu-B fsp-18 color-pidgey text-start lhp-20 psp-35">¡Nos ponemos en acción!</div>
               <div className="d-flex">
-              <img className="hp-20 align-center mt-3 me-2 mb-2" src="images/page_034/g.svg" alt="" />
-                <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">4</div>
-                <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start">Elaboramos un plan para aplicar las encuestas.</div>
+                <img className="hp-15 mtp-20 pep-10" src="images/page_034/g.svg" alt="" />
+                <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey pep-10">4</div>
+                <div className="f-Ubuntu-R fsp-16 text-start pep-10 mtp-15">Elaboramos un plan para aplicar las encuestas.</div>
               </div>
             </div>
 
@@ -94,42 +94,42 @@ function Page034(props) {
               </div>
             </div>
 
-            <div className="ptp-10 psp-110 mbp-0 contenedor-pagina">
+            <div className="ptp-10 psp-85 pep-40">
               <div className="d-flex">
-                <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">5</div>
-                <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start">Aplicamos las encuestas que permitirán conocer la diversidad que existe en nuestra comunidad para considerarla en nuestra fiesta.</div>
+                <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey pep-10">5</div>
+                <div className="f-Ubuntu-R fsp-16 text-start pep-10 mtp-15">Aplicamos las encuestas que permitirán conocer la diversidad que existe en nuestra comunidad para considerarla en nuestra fiesta.</div>
               </div>
             </div>
 
-            <div className="psp-110 mbp-0 contenedor-pagina">
+            <div className="ptp-10 psp-85 pep-40">
               <div className="d-flex">
-                <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey">6</div>
-                <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 text-start">Comprobamos si realizamos bien el trabajo.</div>
+                <div className="f-ccdigitaldelivery-bold fsp-35 color-pidgey pep-10">6</div>
+                <div className="f-Ubuntu-R fsp-16 text-start pep-10 mtp-15">Comprobamos si realizamos bien el trabajo.</div>
               </div>
             </div>
 
-            <div className="psp-140 pep-120 contenedor-pagina position-relative">
-            <div className="position-absolute top-0 start-0 h-100 ptp-15 pbp-15 msp-135"><div className="espiral-calado wp-25 h-100"></div></div>
-            <div className="position-absolute bottom-0 end-0"><img src="images/page_034/mono.png" className="wp-90 mep-80 mtp-65" alt=""/></div>
+            <div className="psp-120 pep-55 pbp-60 position-relative">
+            <div className="position-absolute top-0 start-0 h-100 ptp-15 pbp-15 msp-115"><div className="espiral-calado wp-25 h-100"></div></div>
+            <div className="position-absolute bottom-0 end-0 visible-768"><img src="images/page_034/mono.png" className="wp-90 mep-80 mtp-65" alt=""/></div>
               <div className="w-100 psp-40 ptp-20 pbp-20 text-left bgc-jarek rounded-p-20">
                 <div className="d-flex align-items-center pbp-10">
-                  <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                  <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                   <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Seleccionamos las preguntas para nuestras encuestas.</div>
                 </div>
                 <div className="d-flex align-items-center pbp-10">
-                  <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                  <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                   <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Elaboramos nuestras encuestas.</div>
                 </div>
                 <div className="d-flex align-items-center pbp-10">
-                  <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                  <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                   <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Definimos a quiénes íbamos a aplicar la encuesta.</div>
                 </div>
                 <div className="d-flex align-items-center pbp-10">
-                  <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                  <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                   <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Creamos un plan de acción.</div>
                 </div>
                 <div className="d-flex align-items-center">
-                  <div className="pep-10"><img src="images/page_034/cuadro.svg" className="hp-30" alt="" /></div>
+                  <div className="pep-10"><div className="cuadro-clic"> <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 flex-grow-1 contenedor-linea lhp-30 min-hp-30" /></div></div>
                   <div className="f-Ubuntu-L fsp-15 lh-sm pep-5">Aplicamos las encuestas a quienes correspondía.</div>
                 </div>
               </div>
