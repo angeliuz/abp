39126c973b5817a280a8929f653c79cf586b09ed
ErikPereda <erik.pereda@gmail.com>
2022-01-25 09:17:21 -0300

    erik

--- books/ciencias_sociales/4B/reader/src/pages/page060.jsx
+++ books/ciencias_sociales/4B/reader/src/pages/page060.jsx
@@ -106,8 +106,8 @@ function Page060(props) {
                                     <div className="d-flex flex-wrap w-100 align-items-end titulo-linea mbp-0">
                                         {/* <span className="w-24 f-Ubuntu-L mtp-5 mbp-10">Marco con un <img src="images/generales/ticket.svg" className="hp-17 mtp--3 msp-5 mep-5" alt=""/>el recuadro que mejor me represente.</span> */}
                                         <div className="w-100 p-0 pb-3 position-relative">
-                                            <div className="position-absolute mtp--40 top-0 start-0 msp-10 mtp--5 f-pixilate-bold fsp-18 lh-sm text-start">¿Cómo me siento?</div>
-                                            <div className="position-absolute top-0 start-0 msp-10 mtp--5 f-Ubuntu-R fsp-16  lh-sm text-start">¿Marco la alternativa que más me representa.</div>
+                                            <div className="position-absolute mtp--40 top-0 start-0 msp-10 mtp--10 mbp-10 f-pixilate-bold fsp-18 lh-sm text-start">¿Cómo me siento?</div>
+                                            <div className="d-flex position-absolute align-items-start w-50  mbp-30 top-0 start-0 msp-10 mtp--20 f-Ubuntu-R fsp-15  lh-sm text-start">¿Marco la alternativa que más me representa.</div>
                                             <table className="w-100">
                                                 <tbody>
                                                     <tr>
