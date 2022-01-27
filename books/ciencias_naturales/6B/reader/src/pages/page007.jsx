837b2bbafa0248ee9c2499800f4091ff14152365
clau <claudia.barba@grupo-sm.com>
2022-01-27 17:53:17 -0300

    clau

--- books/ciencias_naturales/6B/reader/src/pages/page007.jsx
+++ books/ciencias_naturales/6B/reader/src/pages/page007.jsx
@@ -97,9 +97,9 @@ function Page007(props) {
 
               {/* BEGIN EJERCICIO 2 */}
 
-              <div className="max-p-700 mx-auto bgc-kokomi  mtp-40 mbp-70 position-relative">
-                <div className="position-absolute top-0 start-100 translate-middle msp-10 mtp-30 visible-768 zindex-1"><img src={"images/page_" + pagina + "/img_005.png"} className="wp-30" alt="" /></div>
-                <div className="text-start ptp-10"> Nos necesitan para...</div>
+              <div className="max-p-700 mx-auto bgc-kokomi mtp-40 mbp-70 position-relative">
+                <div className="position-absolute top-0 start-100 translate-middle msp--50 mtp-0 visible-768 zindex-1"><img src={"images/page_" + pagina + "/img_005.png"} className="wp-30" alt="" /></div>
+                <div className="text-start ptp-10 f-Ubuntu-R fsp-15"> Nos necesitan para...</div>
                 <div className="psp-10 psp-10">
                   <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-170" />
                 </div>
@@ -155,64 +155,7 @@ function Page007(props) {
         </div>
       </div>
     </div>
-    // <div className="wrapper bgc-light">
-    //   <div className="d-flex flex-column align-items-center pbp-20">
-    //     <div className="page bgc-white overflow-hidden position-relative" id={"page_" + pagina}>
-    //       {/* BEGIN CABECERA VERSIÓN 2 */}
-    //       <div className={"cabecera-v2 d-flex flex-column " + background}>
-    //         <div className="d-flex contenedor-cabecera-v2 ptp-40 psp-30 pep-30">
-    //           <div className={"caja wp-150 text-center fsp-24 f-IntroRustG-Base " + color}>SESIÓN 1</div>
-    //           <div className="section-name d-flex align-items-center fsp-24 fw-700 f-Ubuntu-M color-white">¡Tenemos un desafío!</div>
-    //         </div>
 
-    //       </div>
-    //       {/* END CABECERA VERSIÓN 2 */}
-
-
-    //       {/* BEGIN PIE DE PAGINA */}
-    //       <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{ bottom: 70, right: 0 }}>
-    //         <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{ transform: `rotate(270deg)`, color: "dark" }}>
-    //           ABP &copy; SM
-    //         </div>
-    //         <div className="bgc-boros wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">{props.numeroPagina(pagina)}</div>
-    //       </div>
-    //       <div className="row w-100 position-absolute caja-flotante2 p-0 m-0 align-items-end" style={{ bottom: 0 }}>
-    //         <div className="col-7 col-sm-2 p-0 m-0 position-relative">
-    //           <img src="images/rotulo_motivacion.svg" className="visible rotulo-etapa position-absolute top-0 start-0" alt="" />
-    //           <div className="w-100 hp-15 bgc-yamcha rounded-caja-start"></div>
-    //           <div className="w-100 hp-25 bgc-goten"></div>
-    //         </div>
-    //         <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-    //           <img src="images/rotulo_planificacion.svg" className="oculto" alt="" />
-    //           <div className="w-100 hp-15 bgc-tshinhan rounded-caja"></div>
-    //           <div className="w-100 hp-25 bgc-bulma"></div>
-    //         </div>
-    //         <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-    //           <img src="images/rotulo_investigacion.svg" className="oculto" alt="" />
-    //           <div className="w-100 hp-15 bgc-milk rounded-caja"></div>
-    //           <div className="w-100 hp-25 bgc-android18"></div>
-    //         </div>
-    //         <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-    //           <img src="images/rotulo_experimentacion.svg" className="oculto" alt="" />
-    //           <div className="w-100 hp-15 bgc-freezer rounded-caja"></div>
-    //           <div className="w-100 hp-25 bgc-pidgey"></div>
-    //         </div>
-    //         <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-    //           <img src="images/rotulo_creacion.svg" className="oculto" alt="" />
-    //           <div className="w-100 hp-15 bgc-ash rounded-caja"></div>
-    //           <div className="w-100 hp-25 bgc-saitama"></div>
-    //         </div>
-    //         <div className="col-1 col-sm-2 p-0 m-0 position-relative">
-    //           <img src="images/rotulo_difusion.svg" className="oculto" alt="" />
-    //           <div className="w-100 hp-15 bgc-leono rounded-caja-end"></div>
-    //           <div className="w-100 hp-25 bgc-pikachu"></div>
-    //         </div>
-    //       </div>
-    //       {/* END PIE DE PAGINA */}
-
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
