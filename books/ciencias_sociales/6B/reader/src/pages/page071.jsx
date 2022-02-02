cce082fc1f5a4d757bc23084ddfa5aa9f38df9d6
clau <claudia.barba@grupo-sm.com>
2022-02-02 15:03:34 -0300

    clau

--- books/ciencias_sociales/6B/reader/src/pages/page071.jsx
+++ books/ciencias_sociales/6B/reader/src/pages/page071.jsx
@@ -14,172 +14,25 @@ function Page071(props) {
     const borderColor = "border-color-pikachu"
     const borderColorTabla = "border-color-kokomi"
     const bgc_cajas = "bgc-kokomi";
+
     return (
-
-
         <div className="wrapper bgc-light">
-            <div className="d-flex flex-column align-items-center pbp-20">
-                <div className="page bgc-white overflow-hidden position-relative" id={"page_" + pagina}>
+            <div className="d-flex flex-column align-items-center pbp-10">
+                <div className="page position-relative bgc-difusion" id={"page_" + pagina}>
 
-                    {/* BEGIN CABECERA VERSIÓN 2 */}
-                    <div className={"cabecera-v2 d-flex flex-column " + background}>
-                        <div className="d-flex contenedor-cabecera-v2 ptp-40 psp-30 pep-30">
-                            <div className={"caja-seccion wp-160 hp-70 text-center fsp-24 f-IntroRustG-Base color-dark"}>SESIÓN 19</div>
-                            <div className="d-flex align-items-center fsp-24 fw-700 f-Ubuntu-M color-white">Nos presentamos</div>
-                        </div>
-                        <div className="d-flex contenedor-cabecera-v2 ptp-10 psp-30 pep-30 align-self-center justify-content-between w-95">
-                            <div className="psp-5 pep-5 align-self-center f-colby-compres-bold fsp-17 lh-sm text-white text-center">
-                                Mis objetivos en  esta sesión son:
-                            </div>
-                            <div className="p-0 align-self-center">
-                                <img src={"images/generales/flecha_sesion_e" + etapa + "_1.svg"} className="wp-40 arrow-global" alt="" />
-                            </div>
-                            <div className="psp-5 pep-5 align-self-center f-colby-compres-bold fsp-17 lh-sm text-white text-center">
-                                Leer el correo de la organización
-                            </div>
-                            <div className="psp-5 pep-5 align-self-center">
-                                <img src={"images/generales/flecha_sesion_e" + etapa + "_2.svg"} className="wp-40 arrow-global" alt="" />
-                            </div>
-                            <div className="psp-5 pep-5 align-self-center f-colby-compres-bold fsp-17 lh-sm text-white text-center">
-                                Elaborar un discurso para presentar el proyecto a la comunidad educativa
-                            </div>
-                            <div className="p-0 align-self-center">
-                                <img src={"images/generales/flecha_sesion_e" + etapa + "_1.svg"} className="wp-40 arrow-global" alt="" />
-                            </div>
-                            <div className="psp-5 pep-5 align-self-center f-colby-compres-bold fsp-17 lh-sm text-white text-center">
-                                APracticar la  presentación delante de los compañeros
-                            </div>
-                            <div className="p-0 align-self-center">
-                                <img src={"images/generales/flecha_sesion_e" + etapa + "_2.svg"} className="wp-40 arrow-global" alt="" />
-                            </div>
-                            <div className="psp-5 pep-5 align-self-center f-colby-compres-bold fsp-17 lh-sm text-white text-center">
-                                Valorar lo aprendido sobre las presentaciones
-                            </div>
-
+                    <div className="mep-0 position-absolute top-0 end-0">
+                        <div className={"d-flex justify-content-start " + bgc_cajas + " " + color + " ptp-55 pb-2 px-3 text-center mep-55 rounded-seb-20 lh-1"}>
+                            <div className="f-pixilate-regular color-pikachu fsp-25"> APRENDO CON <br />  <span className="f-Ubuntu-B fsp-22">PROYECTOS</span> </div>
                         </div>
                     </div>
-                    {/* END CABECERA VERSIÓN 2 */}
-
-                    <div className="ptp-30 container-titulo-global mbp-0">
-                        <div className="d-flex psp-20">
-                            {/* <img className="hp-20 align-center mt-3 me-2 mb-2" src={"images/page_"+pagina+"/img_.svg"} alt=""  /> */}
-                            <div className={"f-ccdigitaldelivery-bold fsp-35 " + color}>
-                                1
-                            </div>
-                            <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
-                                Leo el correo.
-                            </div>
-                            {/* <img src={"images/page_"+pagina+"/img_001.svg"} className="wp-100" alt=""/> */}
-                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image={"images/generales/play.svg"} clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" /> */}
-                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image={"images/page_"+pagina+"/img_001.png"} className="p-0 px-1" /> */}
-                            {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image={"images/page_"+pagina+"/revista65.svg"} clasesImagen="hp-40 text-center mtp-10 " className="p-0 px-0" /> */}
+                    <div className="position-absolute top-50 start-50 translate-middle">
+                        <div className="fw-700 fsp-80 text-center color-white ft-m-45">
+                            <span className="f-hanoded-butterflyball t-shadow-difusion">DIFUSIÓN</span>
                         </div>
                     </div>
-                    <div class="cb-contenedor-mail position-relative">
-                        <div className="position-absolute top-0 start-100 translate-middle msp--40 mtp--20 visible-768 zindex-10"><img src={"images/page_" + pagina + "/img_003.png"} className="wp-50" alt="" /></div>
-                        <div class="cb-datos-mail pep-15 psp-10 ptp-10 pbp-10">
-                            <div class="d-flex mbp-5">
-                                <div class="cb-datos-a psp-10 f-Ubuntu-R fsp-16 mep-10">De:</div>
-                                <div class="cb-datos-1 psp-10 f-Ubuntu-R fsp-16 text-start">Organización revista sostenibilidad y medioambiente</div>
-                            </div>
-                            <div class="d-flex mbp-5">
-                                <div class="cb-datos-a psp-10 mep-10">Para:</div>
-                                <div class="cb-datos-1 psp-10 f-Ubuntu-R fsp-16 text-start">Equipos participantes</div>
-                            </div>
-                            <div class="d-flex">
-                                <div class="cb-datos-a psp-10 f-Ubuntu-R fsp-16 mep-10">Asunto:</div>
-                                <div class="cb-datos-1 psp-10 f-Ubuntu-R fsp-16 text-start">¡Llegó el día!</div>
-                            </div>
-                        </div>
-                        <div class="cb-texto-mail position-relative">
-                            <div class="cb-carta-content f-Ubuntu-L text-start">
-                                <div className="position-absolute top-50 start-100 translate-middle msp-0 mtp--60 visible-768 zindex-1"><img src={"images/page_" + pagina + "/img_004.png"} className="wp-20" alt="" /></div>
-                                <div className="position-absolute top-50 start-0 translate-middle msp-0 mtp-70 visible-768 zindex-1"><img src={"images/page_" + pagina + "/img_005.png"} className="wp-60" alt="" /></div>
-                                <p>Estimados estudiantes:</p>
-                                <p>Nos ha llegado información sobre las soluciones ingeniosas que han
-                                    encontrado para afrontar el desafío. Por ello, los queremos felicitar
-                                    por construir el aparato/sistema tecnológico que aprovechará la
-                                    energía renovable en su colegio.</p>
-
-                                <p>Sabemos que el trabajo ha sido exigente, y que han debido enfrentar
-                                    diversos obstáculos, no obstante, han logrado superarlos con éxito
-                                    y ahora solo les queda presentar, a la comunidad educativa, sus proyectos.</p>
-
-
-                                <p> ¡No olviden disfrutar de esta etapa final!</p>
-                                <p> Saludos afectuosos.  </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="container-titulo-global mbp-10">
-                        <div className="d-flex psp-20">
-                            {/* <img className="hp-20 align-center mt-3 me-2 mb-2" src={"images/page_"+pagina+"/img_.svg"} alt=""  /> */}
-                            <div className={"f-ccdigitaldelivery-bold fsp-35 " + color}>
-                                2
-                            </div>
-                            <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
-                                Veo el video y escribo mi conclusión.
-                            </div>
-                            <ModalVideo id={"p" + pagina + "_video" + (indexInput += 1)} image={"images/generales/play.svg"} clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" />
-                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image={"images/page_"+pagina+"/img_001.png"} className="p-0 px-1" /> */}
-                            {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image={"images/page_"+pagina+"/revista65.svg"} clasesImagen="hp-40 text-center mtp-10 " className="p-0 px-0" /> */}
-                        </div>
-                    </div>
-                    {/* INICIO EJERCICIO 2 */}
-                    <div className="max-wp-700 mx-auto border-1 border-style-solid border-color-pikachu mbp-90 bgc-kokomi cb-outline-p71">
-                        <div className="pep-10 psp-10">
-                            <div className="text-start ptp-10">Tengo claro que</div>
-                            <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-170" />
-                        </div>
-                    </div>
-
-
-                    {/* BEGIN PIE DE PAGINA */}
-                    <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{ bottom: 70, right: 0 }}>
-                        <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{ transform: `rotate(270deg)`, color: "dark" }}>
-                            ABP &copy; SM
-                        </div>
-                        <div className="bgc-boros wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">{props.numeroPagina(pagina)}</div>
-                    </div>
-                    <div className="row position-absolute caja-flotante2 p-0 m-0 align-items-end" style={{ bottom: 0 }}>
-
-                        <div className={etapa == 1 ? "col-7 position-relative col-sm-2 p-0 m-0" : "col-1 position-relative col-sm-2 p-0 m-0"}>
-                            <img src="images/rotulo_motivacion.svg" className={etapa == 1 ? "visible rotulo-etapa position-absolute top-0 start-0" : "oculto"} alt="" />
-                            <div className="w-100 hp-15 bgc-yamcha rounded-caja-start"></div>
-                            <div className="w-100 hp-25 bgc-goten"></div>
-                        </div>
-                        <div className={etapa == 2 ? "col-7 position-relative col-sm-2 p-0 m-0" : "col-1 position-relative col-sm-2 p-0 m-0"}>
-                            <img src="images/rotulo_planificacion.svg" className={etapa == 2 ? "visible rotulo-etapa position-absolute top-0 start-0" : "oculto"} alt="" />
-                            <div className="w-100 hp-15 bgc-tshinhan rounded-caja"></div>
-                            <div className="w-100 hp-25 bgc-bulma"></div>
-                        </div>
-                        <div className={etapa == 3 ? "col-7 position-relative col-sm-2 p-0 m-0" : "col-1 position-relative col-sm-2 p-0 m-0"}>
-                            <img src="images/rotulo_investigacion.svg" className={etapa == 3 ? "visible rotulo-etapa position-absolute top-0 start-0" : "oculto"} alt="" />
-                            <div className="w-100 hp-15 bgc-milk rounded-caja"></div>
-                            <div className="w-100 hp-25 bgc-android18"></div>
-                        </div>
-                        <div className={etapa == 4 ? "col-7 position-relative col-sm-2 p-0 m-0" : "col-1 position-relative col-sm-2 p-0 m-0"}>
-                            <img src="images/rotulo_experimentacion.svg" className={etapa == 4 ? "visible rotulo-etapa position-absolute top-0 start-0" : "oculto"} alt="" />
-                            <div className="w-100 hp-15 bgc-freezer rounded-caja"></div>
-                            <div className="w-100 hp-25 bgc-pidgey"></div>
-                        </div>
-                        <div className={etapa == 5 ? "col-7 position-relative col-sm-2 p-0 m-0" : "col-1 position-relative col-sm-2 p-0 m-0"}>
-                            <img src="images/rotulo_creacion.svg" className={etapa == 5 ? "visible rotulo-etapa position-absolute top-0 start-0" : "oculto"} alt="" />
-                            <div className="w-100 hp-15 bgc-ash rounded-caja"></div>
-                            <div className="w-100 hp-25 bgc-saitama"></div>
-                        </div>
-                        <div className={etapa == 6 ? "col-7 position-relative col-sm-2 p-0 m-0" : "col-1 position-relative col-sm-2 p-0 m-0"}>
-                            <img src="images/rotulo_difusion.svg" className={etapa == 6 ? "visible rotulo-etapa position-absolute top-0 start-0" : "oculto"} alt="" />
-                            <div className="w-100 hp-15 bgc-leono rounded-caja-end"></div>
-                            <div className="w-100 hp-25 bgc-pikachu"></div>
-                        </div>
-                    </div>
-                    {/* END PIE DE PAGINA */}
-
                 </div>
             </div>
         </div>
-
     );
 }
 
