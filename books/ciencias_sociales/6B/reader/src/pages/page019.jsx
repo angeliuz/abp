d6ba03e6f9f42af09abec719ab568aafedfbbe71
AldoJGonzalez <aldo.gonzalez@colex.grupo-sm.com>
2022-02-02 14:25:39 -0300

    Aldo 2Feb

--- books/ciencias_sociales/6B/reader/src/pages/page019.jsx
+++ books/ciencias_sociales/6B/reader/src/pages/page019.jsx
@@ -8,30 +8,65 @@ function Page019(props) {
 
   const pagina = "019";
   var indexInput = 0;
-  const etapa = "3"
-  const background = "bgc-android18";
-  const color = "color-primary";
-  const borderColor = "border-color-android18"
-  const bgc_cajas = "bgc-goku";
+  const etapa = "2"
+  const background = "bgc-bulma";
+  const color = "color-bulma";
+  const borderColor = "border-color-bulma"
+  const bgc_cajas = "bgc-draco";
+  const borderColorTabla = "border-color-buzz";
 
   return (
-
     <div className="wrapper bgc-light">
-        <div className="d-flex flex-column align-items-center pbp-10">
-        <div className="page position-relative bgc-investigacion" id={"page_"+pagina}>
+      <div className="d-flex flex-column align-items-center pbp-20">
+        <div className={"dosier-e" + etapa + " page bgc-white overflow-hidden position-relative"} id={"page_" + pagina}>
+          <img src={"images/cabecera/dosier_e" + etapa + ".svg"} className="w-100" alt="" />
+          <div className="position-relative w-100 mbp-10">
+            <div className="psp-80 pep-70 ptp-30 pbp-0 position-relative">
+              <div className="fsp-16 f-Ubuntu-R text-start psp-50 pbp-10">Esta página fue completada por:</div>
+              <div className={"psp-50 pep-50 ptp-5 pbp-5 bgc-white rounded-p-10 border-style-solid border-1 " + borderColor + " d-flex justify-content-center position-relative"}>
+                <div className="position-absolute top-50 start-0 translate-middle msp-0 mtp--10 zindex-1"><img src={"images/page_" + pagina + "/img_001.png"} className="wp-100" alt="" /></div>
+                <div className="position-absolute top-50 start-100 translate-middle wp-100 msp--15 mtp-5"><img src={"images/page_" + pagina + "/img_002.png"} className="w-80" alt="" /></div>
+                <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-35" />
+              </div>
+            </div>
+          </div>
 
-                <div className="mep-0 position-absolute top-0 end-0">
-                        <div className={"d-flex justify-content-start "+bgc_cajas+" "+color+" ptp-55 pb-2 px-3 text-center mep-55 rounded-seb-20 lh-1"}>
-                        <div className="f-pixilate-regular fsp-25"> APRENDO CON <br/>  <span className="f-Ubuntu-B fsp-22">PROYECTOS</span> </div>
-                        </div>
-                </div>
-                <div className="position-absolute top-50 start-50 translate-middle">
-                    <div className="fw-700 fsp-80 text-center color-white ft-m-45">
-                        <span className="f-hanoded-butterflyball t-shadow-investigacion">INVESTIGACIÓN</span>
+          <div className="ptp-20 pbp-20 container-titulo-global mbp-0">
+            <div className="d-flex psp-20">
+              {/* <img className="hp-20 align-center mt-3 me-2 mb-2" src={"images/page_"+pagina+"/img_.svg"} alt=""  /> */}
+              <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
+                Vemos el video. Luego, leemos y numeramos las tareas del proyecto.
+              </div>
+              <ModalVideo id={"p" + pagina + "_video" + (indexInput += 1)} image={"images/generales/play.svg"} clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" />
+              {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image={"images/page_"+pagina+"/img_001.png"} className="p-0 px-1" /> */}
+              {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image={"images/page_"+pagina+"/revista65.svg"} clasesImagen="hp-40 text-center mtp-10 " className="p-0 px-0" /> */}
+            </div>
+          </div>
+
+          <div className="w-100 h-100 position-relative">
+            <div className="msp-80 mep-60 position-relative ">
+              <div className="position-absolute top-50 start-0 translate-middle zindex-2 msp--45 "> <img src={"images/page_" + pagina + "/img_005.png"} className="" alt="" />  </div>
+              <div className="row w-100 ">
+                <div className="cb-ep-paper-29 ">
+                  <div className="cb-ep-paper-content-29 f-Ubuntu-L text-start">
+                    <div className="mtp-5 ">
                     </div>
+                  </div>
                 </div>
+              </div>
+
+            </div>
+
+          </div>
+
+          {/* BEIGN PIE DE PAGINA */}
+          <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{ bottom: 70, right: 0 }}>
+            <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{ transform: `rotate(270deg)`, color: "dark" }}>ABP &copy; SM</div>
+            <div className="bgc-goku wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">{props.numeroPagina(pagina)}</div>
+          </div>
+          {/* END PIE DE PAGINA */}
         </div>
-        </div>
+      </div>
     </div>
 
   );
