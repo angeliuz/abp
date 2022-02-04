7380272aa4cef7e442bc29a97c07fa6f469d8ad4
cristian1562 <cristian.acevedo@grupo-sm.com>
2022-02-04 16:08:07 -0300

    cristian

--- books/ciencias_naturales/5B/reader/src/pages/page036.jsx
+++ books/ciencias_naturales/5B/reader/src/pages/page036.jsx
@@ -13,106 +13,114 @@ function Page036(props) {
     const color = "color-android18";
     const colorSesion = "color-primary";
     const borderColor = "border-color-android18"
-    const bgc_cajas = "bgc-azuka";
+    const bgc_cajas = "bgc-goku";
 
 
     return (
-
         <div className="wrapper bgc-light">
             <div className="d-flex flex-column align-items-center pbp-20">
-                <div className={"dosier-e" + etapa + " page bgc-white overflow-hidden position-relative"} id={"page_" + pagina}>
-
-
-
-
-                    <div className="d-flex msp-60 mep-60 mbp-0 mtp-90">
-                        <div className="row w-100 p-0 m-0">
-
-                            <div className="col-sm-12 col-md-6 position-relative">
-
-                                <div className="bgc-white rounded-p-10 border-style-solid border-color-raichu border-5 w-100 p-2 f-Ubuntu-L fsp-15 mbp-20">
-                                    <div className="d-flex flex-column position-relative w-100 text-start ">
-                                        <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-400" />
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col-sm-12 col-md-6 position-relative">
-
-                                <div className="position-relative">
-
-                                    <div className="position-absolute top-0 start-50 translate-middle w-100 msp-0 mtp-0 lhp-17 d-flex center-center">
-                                        <div className="bgc-pantro rounded-p-10 w-70 psp-10 pep-10 ptp-10 pbp-10">
-                                            <span className="color-dark f-Ubuntu-M fsp-15">Xx xxxxx xxxx xxxx xxx xxxx.</span>
-                                        </div>
-                                    </div>
-
-                                    <div className="bgc-white rounded-p-10 border-style-solid border-color-pantro border-5 w-100 p-2 f-Ubuntu-L fsp-15 mbp-20">
-                                        <div className="d-flex flex-column position-relative w-100 text-start mtp-10">
-                                            <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-200" />
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                            </div>
-
-
-                        </div>
-
+                <div className="page bgc-white overflow-hidden position-relative" id={"page_" + pagina}>
+                    <div className="cabecera-v1 hp-80 bgc-android18">
                     </div>
 
-                    <div className="d-flex flex-column  mbp-100 mtp-0">
+                    <div className="ptp-30 container-titulo-global mbp-10">
+                        {/* <div className={"f-Ubuntu-B fsp-20 psp-0 text-start lhp-20 "+color}>Ahora podremos comprobar qué ocurre en nuestro entorno</div> */}
+                        <div className="d-flex">
+                            {/* <img className="hp-20 align-center mt-3 me-2 mb-2" src={"images/page_"+pagina+"/img_005.svg"} alt=""  /> */}
+                            <div className={"f-ccdigitaldelivery-bold fsp-35 " + color}>3</div>
+                            <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
+                                Hago el boceto de mi infografía.
+                            </div>
+                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/generales/play.svg" clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" /> */}
+                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_007/img_001.png" className="p-0 px-1" /> */}
+                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_007/img_001.png" className="p-0 px-1" /> */}
+                            {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image={"images/page_"+pagina+"/img_002.svg"} clasesImagen="hp-40 text-center mtp-10 " className="p-0 px-0" /> */}
+                        </div>
+                    </div>
 
+                    <div className="d-flex global-margin mtp-0 mbp-0 psp-0 pep-0 ptp-0 pbp-0">
+                        <div className="row p-0 m-0 w-100">
+                            <div className="col-12 p-0 m-0 position-relative">
+                                <div className="position-absolute top-0 start-50 translate-middle mtp--50 msp--30 visible-768"><img src="images/page_036/img_001.png" className="" alt="" /></div>
+                                <div className="position-absolute top-50 start-0 translate-middle  zindex-1 msp-20 mtp--5"><img src="images/page_036/img_002.png" className="w-100" alt="" /></div>
+                                <div className="position-absolute top-100 start-100 translate-middle mtp--130 msp--20 visible-768"><img src="images/page_036/img_003.png" className="wp-150" alt="" /></div>
 
-
-                        <div className="d-flex flex-column global-margin mtp-0 mbp-20 psp-0 pep-0 ptp-0 pbp-0 position-relative">
-                            <div className="position-absolute top-0 start-100 translate-middle msp--100 mtp-5 zindex-1"><img src={"images/page_" + pagina + "/img_006.png"} className="wp-180" alt="" /></div>
-                            <div className="f-pixilate-bold fsp-18 lh-sm text-start mbp-10">¿Cómo voy?</div>
-                            <div className="d-flex flex-column bgc-white rounded-p-10 psp-10 pep-10 ptp-10 pbp-10 mtp-0 mbp-0">
-                                <div className="row p-0 m-0 w-100">
-                                    <div className="col-12 p-0 m-0">
-                                        <div className="d-flex flex-column">
-                                            <div className="d-flex align-items-start">
-                                                <div className="mtp-0 pep-5">
-                                                    <img src={"images/generales/flecha_e" + etapa + ".svg"} className="wp-15" />
-                                                </div>
-                                                <div className="f-Ubuntu-L fsp-15 ptp-2 text-start">  Lo que más nos ha costado del trabajo en equipo es...</div>
-                                            </div>
-                                            <div className="d-flex w-100 psp-20 pep-10 ptp-0 pbp-0">
-                                                <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-80" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-12 p-0 m-0">
-                                        <div className="d-flex flex-column">
-                                            <div className="d-flex align-items-start">
-                                                <div className="mtp-0 pep-5">
-                                                    <img src={"images/generales/flecha_e" + etapa + ".svg"} className="wp-15" />
-                                                </div>
-                                                <div className="f-Ubuntu-L fsp-15 ptp-2 text-start">  Lo podemos mejorar...</div>
-                                            </div>
-                                            <div className="d-flex w-100 psp-20 pep-10 ptp-0 pbp-0">
-                                                <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-80" />
-                                            </div>
-                                        </div>
+                                <div className="mbp-20 mtp-10 msp-20 mep-20">
+                                    <div className=" bgc-white caja-dibujo-global p-2 rounded-p-20 border-style-solid border-2 border-color-dark">
+                                        <InputBox id={"p" + pagina + "_input" + (indexInput += 1)} className="p-0 px-1 text-start flex-grow-1 contenedor-linea lhp-35 min-hp-780" />
                                     </div>
                                 </div>
+
+
                             </div>
                         </div>
-
-
-
                     </div>
 
 
-                    {/* PIE DE PAGINA */}
+                    <div className="ptp-30 container-titulo-global mbp-90">
+                        {/* <div className={"f-Ubuntu-B fsp-20 psp-0 text-start lhp-20 "+color}>Ahora podremos comprobar qué ocurre en nuestro entorno</div> */}
+                        <div className="d-flex">
+                            <img className="hp-20 align-center mt-3 me-2 mb-2" src={"images/page_"+pagina+"/img_002.svg"} alt=""  />
+                            <div className={"f-ccdigitaldelivery-bold fsp-35 " + color}>4</div>
+                            <div className="f-Ubuntu-R fsp-16 ptp-18 psp-10 pep-10 text-start">
+                                Muestro a mis compañeros de equipo mi boceto y observo los de ellos. Votamos
+                                por el que más nos gustó y lo traspasamos a una cartulina para crear una
+                                infografía que incorporaremos a nuestra campaña.
+                            </div>
+                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/generales/play.svg" clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" /> */}
+                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_007/img_001.png" className="p-0 px-1" /> */}
+                            {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image="images/page_007/img_001.png" className="p-0 px-1" /> */}
+                            {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image={"images/page_"+pagina+"/img_002.svg"} clasesImagen="hp-40 text-center mtp-10 " className="p-0 px-0" /> */}
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+                    {/* BEGIN PIE DE PAGINA */}
                     <div className="d-flex hp-90 caja-flotante p-0 m-0 position-absolute justify-content-end align-items-end zindex-2" style={{ bottom: 70, right: 0 }}>
                         <div className="f-Ubuntu-M fsp-10 position-absolute mbp-60" style={{ transform: `rotate(270deg)`, color: "dark" }}>
                             ABP &copy; SM
                         </div>
                         <div className="bgc-boros wp-50 hp-30 f-Ubuntu-M fsp-20 psp-5">{props.numeroPagina(pagina)}</div>
                     </div>
+                    <div className="row w-100 position-absolute caja-flotante2 p-0 m-0 align-items-end" style={{ bottom: 0 }}>
+                        <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                            <img src="images/rotulo_motivacion.svg" className="oculto" alt="" />
+                            <div className="w-100 hp-15 bgc-yamcha rounded-caja-start"></div>
+                            <div className="w-100 hp-25 bgc-goten"></div>
+                        </div>
+                        <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                            <img src="images/rotulo_planificacion.svg" className="oculto" alt="" />
+                            <div className="w-100 hp-15 bgc-tshinhan rounded-caja"></div>
+                            <div className="w-100 hp-25 bgc-bulma"></div>
+                        </div>
+                        <div className="col-7 col-sm-2 p-0 m-0 position-relative">
+                            <img src="images/rotulo_investigacion.svg" className="visible rotulo-etapa position-absolute top-0 start-0" alt="" />
+                            <div className="w-100 hp-15 bgc-milk rounded-caja"></div>
+                            <div className="w-100 hp-25 bgc-android18"></div>
+                        </div>
+                        <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                            <img src="images/rotulo_experimentacion.svg" className="oculto" alt="" />
+                            <div className="w-100 hp-15 bgc-freezer rounded-caja"></div>
+                            <div className="w-100 hp-25 bgc-pidgey"></div>
+                        </div>
+                        <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                            <img src="images/rotulo_creacion.svg" className="oculto" alt="" />
+                            <div className="w-100 hp-15 bgc-ash rounded-caja"></div>
+                            <div className="w-100 hp-25 bgc-saitama"></div>
+                        </div>
+                        <div className="col-1 col-sm-2 p-0 m-0 position-relative">
+                            <img src="images/rotulo_difusion.svg" className="oculto" alt="" />
+                            <div className="w-100 hp-15 bgc-leono rounded-caja-end"></div>
+                            <div className="w-100 hp-25 bgc-pikachu"></div>
+                        </div>
+                    </div>
+                    {/* END PIE DE PAGINA */}
 
                 </div>
             </div>
