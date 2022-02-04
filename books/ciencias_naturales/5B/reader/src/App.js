1f02af939bbd415e30db397fb222c657d1b1ac93
clau <claudia.barba@grupo-sm.com>
2022-02-04 16:22:09 -0300

    clau

--- books/ciencias_naturales/5B/reader/src/App.js
+++ books/ciencias_naturales/5B/reader/src/App.js
@@ -73,6 +73,8 @@ import Page073 from "./pages/page073";
 import Page074 from "./pages/page074";
 import Page075 from "./pages/page075";
 import Page076 from "./pages/page076";
+import Page077 from "./pages/page077";
+import Page078 from "./pages/page078";
 
 
 
@@ -180,7 +182,8 @@ function App() {
       <Page074 numeroPagina={clearZeros} />
       <Page075 numeroPagina={clearZeros} />
       <Page076 numeroPagina={clearZeros} />
-
+      <Page077 numeroPagina={clearZeros} />
+      <Page078 numeroPagina={clearZeros} />
     </div>
   );
 }
