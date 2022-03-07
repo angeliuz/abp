bf7396c6088d02f34298d217b18b9d899031c52d
cristian1562 <cristian.acevedo@grupo-sm.com>
2022-03-07 11:12:22 -0300

    cristian

--- books/ciencias_sociales/6B/reader/src/pages/page025.jsx
+++ books/ciencias_sociales/6B/reader/src/pages/page025.jsx
@@ -283,7 +283,7 @@ function Page025(props) {
               <div className="w-100 hp-15 bgc-yamcha rounded-caja-start"></div>
               <div className="w-100 hp-25 bgc-goten"></div>
             </div>
-            <div className="col-7 position-relative col-sm-2 p-0 m-0">
+            <div className="col-1 position-relative col-sm-2 p-0 m-0">
               <img
                 src="images/rotulo_planificacion.svg"
                 className="oculto"
@@ -292,7 +292,7 @@ function Page025(props) {
               <div className="w-100 hp-15 bgc-tshinhan rounded-caja"></div>
               <div className="w-100 hp-25 bgc-bulma"></div>
             </div>
-            <div className="col-1 position-relative col-sm-2 p-0 m-0">
+            <div className="col-7 position-relative col-sm-2 p-0 m-0">
               <img
                 src="images/rotulo_investigacion.svg"
                 className="visible rotulo-etapa position-absolute top-0 start-0"
