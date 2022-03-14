2065347885f8078ef86b79456dfe2da0464d5df0
cristian acevedo <cristian.acevedo@grupo-sm.com>
2022-03-14 15:13:20 -0300

    cristian

--- books/ciencias_naturales/5B/reader/src/pages/page071.jsx
+++ books/ciencias_naturales/5B/reader/src/pages/page071.jsx
@@ -61,7 +61,16 @@ function Page071(props) {
                 Veo el video, leo el mail y luego respondo
               </div>
               {/* <img src={"images/page_"+pagina+"/img_001.svg"} className="wp-100" alt=""/> */}
-              <ModalVideo id={"p" + pagina + "_video" + (indexInput += 1)} image={"images/generales/play.svg"} clasesImagen="cambio hp-40 mtp-10" className="p-0 px-1" />
+              <ModalVideo
+                                            id={"p" + pagina + "_video" + (indexInput += 1)}
+                                            linkVideo="682623207"
+                                            tituloVideo="¡Arcoíris en tu plato! III"
+                                            tipoVideo="vimeo"
+                                            image={"images/generales/play.svg"}
+                                            colorUnidad={background}
+                                            clasesImagen="cambio hp-40 mtp-10"
+                                            className="p-0 px-1"
+                                        />
               {/* <ModalVideo id={ "p" + pagina + "_video"+ (indexInput+=1) } image={"images/page_"+pagina+"/img_001.png"} className="p-0 px-1" /> */}
               {/* <ModalBook id={ "p" + pagina + "_book"+ (indexInput+=1) } image={"images/page_"+pagina+"/revista65.svg"} clasesImagen="hp-40 text-center mtp-10 " className="p-0 px-0" /> */}
             </div>
