2149b90f4cbdd1350976eb6e06093023fd19f06d
Aldo Gonzalez <aldo.gonzalez@colex.grupo-sm.com>
2022-03-14 16:43:56 -0300

    Aldo 14marzo

--- books/ciencias_sociales/4B/reader/src/components/ModalRecortable.js
+++ books/ciencias_sociales/4B/reader/src/components/ModalRecortable.js
@@ -17,51 +17,19 @@ function ModalRecortable(props) {
   const id = props.id;
   const clasesImagen = props.clasesImagen;
   const pagina = props.pagina;
+  const numRecortables = props.numRecortables;
 
 
   const [imagenSeleccionada, setImagenSeleccionada] = useState(image);
   const [content1, setContent1] = useState(image);
 
 
-  console.log("revista pagina: " + id);
+  //console.log("revista pagina: " + id);
 
   useEffect(() => {
     getData();
   }, [imagenSeleccionada, content1]);
 
-  // const getData = () => {
-  //   const obtenerDatos = async () => {
-  //     const docSnap = await getDoc(docRef);
-  //     if(docSnap.exists()){
-  //       onSnapshot(doc(db, coleccion, documento), (doc) => {
-  //         const field = doc.data()[id];
-  //         //console.log("Current data: ", doc.data()[id]);
-  //         if (field) {
-  //           setContent1(doc.data()[id]);
-  //           setImagenSeleccionada(content1);
-  //           //console.log("content1: " + content1);
-  //         } else {
-  //           console.log("Sin datos: "+id);
-  //         }
-  //       });
-
-  //         if (docSnap.data()[id] !== undefined) {
-  //           setContent1(docSnap.data()[id]);
-  //         } else {
-  //           setContent1(image);
-  //           await updateDoc(docRef, {
-  //             [id]: content1,
-  //           });
-  //           console.log("undefined: "+id);
-  //         }
-  //     }else{
-  //       await setDoc(doc(db, coleccion, documento),{[id]:""});
-  //     }
-  //       // console.log("useEffect: " + docSnap.data()[id]);
-  //       // console.log("change: " + content1);
-  //   };
-  //   obtenerDatos();
-  // }
 
   const getData = () => {
     const obtenerDatos = async () => {
@@ -69,11 +37,11 @@ function ModalRecortable(props) {
 
       if (docSnap.exists()) {
         const field = docSnap.data()[id];
-        console.log("Current data: ", docSnap.data()[id]);
+        //console.log("Current data: ", docSnap.data()[id]);
         if (field) {
           setContent1(docSnap.data()[id]);
           setImagenSeleccionada(content1);
-          console.log("content1: " + content1);
+          //console.log("content1: " + content1);
         } else {
           console.log("Sin datos: " + id);
         }
@@ -94,10 +62,10 @@ function ModalRecortable(props) {
     image = "images/page_" + pagina + "/recortables/" + event.target.id + ".png"
     // document.getElementById("recortable_"+props.id).childNodes[0].src = getChildImage(id);
 
-    console.log("click get image: " + getChildImage(event.target.id))
-    console.log("click target: " + event.target.id)
-    console.log("click id: " + id)
-    console.log("click image: " + image)
+    // console.log("click get image: " + getChildImage(event.target.id))
+    // console.log("click target: " + event.target.id)
+    // console.log("click id: " + id)
+    // console.log("click image: " + image)
 
     setContent1(image);
     setImagenSeleccionada(content1);
@@ -130,7 +98,7 @@ function ModalRecortable(props) {
 
   function loopColumn() {
     const column = [];
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= numRecortables; i++) {
       column.push(
 
         <div className="col p-0 m-0 position-relative d-flex center-center p-2 pe-auto" id={"reco_" + i} onClick={handleClick} data-bs-dismiss="modal" >
@@ -155,43 +123,18 @@ function ModalRecortable(props) {
     return num;
   }
 
-
-
-  const styles = {
-    modalHeader: {
-      backgroundColor: "#49B2F4",
-      paddingLeft: "10px",
-      paddingRight: "10px",
-      paddingTop: "5px",
-      paddingBottom: "5px",
-      borderRadius: "9px 9px 0px 0px"
-
-    },
-    modalBody: {
-      margin: "0px",
-      padding: "0px",
-      borderRadius: "15px",
-      minHeight: "400px",
-      maxHeight: "400px",
-      borderRadius: "0px 0px 10px 10px"
-    },
-    modalTitle: {
-      fontSize: "17px",
-    }
-  };
-
-
   return (
     <>
-      <div className="bgc-white mxwp-300 hp-200 centrar position-relative" data-bs-toggle="modal" data-bs-target={"#" + id} id={"recortable_" + id}>
-        <img src={imagenSeleccionada} className="position-absolute top-50 start-50 translate-middle w-100 h-100" alt="" />
+      <div className="bgc-white centrar position-relative" data-bs-toggle="modal" data-bs-target={"#" + id} id={"recortable_" + id}>
+        <img src={imagenSeleccionada} className="w-100 h-100" alt="" />
       </div>
       <div className="modal fade" id={id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
           <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">{"Recortables página " + removeZeros(pagina) + id}</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal-header bgc-info color-white fsp-18 f-Ubuntu-M">
+              <div className="modal-title" id="exampleModalLabel">{"Recortables página " + removeZeros(pagina)}</div>
+
+              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
               <div className="row w-100">
