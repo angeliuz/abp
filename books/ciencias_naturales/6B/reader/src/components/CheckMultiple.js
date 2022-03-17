edb68badef63555a5386ab32b311c25579319fda
cristian1562 <cristian.acevedo@grupo-sm.com>
2022-03-17 09:23:19 -0300

    cristian

--- books/ciencias_naturales/6B/reader/src/components/CheckMultiple.js
+++ books/ciencias_naturales/6B/reader/src/components/CheckMultiple.js
@@ -1,10 +1,6 @@
 import React, { useEffect } from 'react';
 import { useState } from "react";
-<<<<<<< HEAD
-import { doc, getDoc, setDoc, updateDoc, onSnapshot } from "firebase/firestore";
-=======
 import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
 import db from "../firebase/firebaseConfig";
 
 const CheckMultiple = (props) => {
@@ -17,60 +13,17 @@ const CheckMultiple = (props) => {
     const docRef = doc(db, coleccion, documento);
 
 
-<<<<<<< HEAD
-    const className = props.className;
-=======
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
     const image0 = props.image0;
     const image1 = props.image1;
     const image2 = props.image2;
     const id = props.id;
-<<<<<<< HEAD
-    const clasesImagen = props.clasesImagen;
-    const pagina = props.pagina;
-
-    const [seleccion, setSeleccion] = useState();
-=======
     const classContainer = props.classContainer;
 
     const [seleccion, setSeleccion] = useState(1);
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
     const [seleccionImagen, setSeleccionImagen] = useState(image0);
 
 
     // firestore get data for check
-<<<<<<< HEAD
-    useEffect(async() => {
-        getData();
-        
-    }, []);
-
-    useEffect(async () => {
-        console.log('App comp value:', seleccion);
-        updateContenido(seleccion);
-      }, [seleccion]);
-
-    const getData = () => {
-        const obtenerDatos = async () => {
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-            const field = docSnap.data()[id];
-            console.log("Current data: ", docSnap.data()[id]);
-            if (field) {
-               setSeleccion(docSnap.data()[id]);
-
-                console.log("content1: " + seleccion);
-            } else {
-                console.log("Sin datos: "+id);
-            }
-            //console.log("Document data:", docSnap.data());
-        } else {
-            // doc.data() will be undefined in this case
-            await setDoc(doc(db, coleccion, documento), { build: "1" });
-            console.log("No such document!");
-        }
-=======
     useEffect(async () => {
         getData();
 
@@ -101,7 +54,6 @@ const CheckMultiple = (props) => {
                 await setDoc(doc(db, coleccion, documento), { build: "1" });
                 console.log("No such document!");
             }
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
         };
         obtenerDatos();
     }
@@ -115,59 +67,32 @@ const CheckMultiple = (props) => {
     };
 
 
-<<<<<<< HEAD
-
-
-
-    function handleClick() {
-        console.log("seleccion: "+ seleccion)
-        if(seleccion === NaN){
-=======
     function handleClick() {
         console.log("seleccion: " + seleccion)
         if (seleccion === NaN) {
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
             setSeleccion(1);
         }
 
         setSeleccion(seleccion + 1);
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
         if (seleccion > 2) {
             setSeleccion(1);
         }
 
-<<<<<<< HEAD
-        
-        console.log("seleccion: "+ seleccion)
-        
-=======
         updateContenido(seleccion);
         console.log("seleccion: " + seleccion)
 
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
     }
 
     const updateContenido = (data) => {
         if (data === 1) {
             setSeleccionImagen(image0);
-<<<<<<< HEAD
-            
-=======
 
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
         }
 
         if (data === 2) {
             setSeleccionImagen(image1);
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
         }
 
         if (data === 3) {
@@ -179,24 +104,10 @@ const CheckMultiple = (props) => {
         });
     }
 
-<<<<<<< HEAD
-
-    
-
-
-    console.log(seleccion)
-
-    return <div>
-
-        <div className="wp-30 hp-30 bgc-white border-style-solid border-1 border-color-dark bgc-white rounded-1 disable-select d-flex center-center" onClick={handleClick}>
-
-            <img src={seleccionImagen} className={" disable-select"} alt="" />
-=======
     return <div>
 
         <div className={classContainer} onClick={handleClick}>
             <img src={seleccionImagen} className={"disable-select userselect"} alt="" />
->>>>>>> 04dd541f684fa8ede5a497f30de4f1c750c8c845
         </div>
 
     </div>;
