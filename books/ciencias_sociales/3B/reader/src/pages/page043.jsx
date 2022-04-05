531dc03b5b3b28252ea186eb163b0930e3d38b26
cristian acevedo <cristian.acevedo@grupo-sm.com>
2022-04-05 10:47:51 -0400

    cristian

--- books/ciencias_sociales/3B/reader/src/pages/page043.jsx
+++ books/ciencias_sociales/3B/reader/src/pages/page043.jsx
@@ -20,7 +20,7 @@ function Page043(props) {
           {/* BEGIN CABECERA VERSIÓN 2 */}
           <div className="cabecera-v2 bgc-saitama d-flex flex-column">
             <div className="d-flex contenedor-cabecera-v2 ptp-40 psp-30 pep-30">
-              <div className="caja wp-150 text-center fsp-24 f-IntroRustG-Base color-saitama">SESIÓN 11</div>
+              <div className="caja-seccion wp-160 hp-60 d-flex justify-content-center ptp-10 mtp-5 color-saitama fsp-24 f-IntroRustG-Base">SESIÓN 11</div>
               <div className="section-name d-flex align-items-center fsp-24 fw-700 f-Ubuntu-M color-white">Tomamos decisiones</div>
             </div>
 
