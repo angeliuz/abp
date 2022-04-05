72e04d230cfd1927904ea1f34a00933726cd0602
Jorge Espinoza <jorge.espinoza@grupo-sm.com>
2022-04-05 18:15:32 -0400

    jorge

--- books/libro_general/reader/src/components/Sopa1.js
+++ books/libro_general/reader/src/components/Sopa1.js
@@ -12,12 +12,24 @@ function Sopa1(props) {
     }, []);
 
 
+    function handleEvent(event) {
+        console.log(event.target.id);
+        if(event.target.style.backgroundColor == "red"){
+            event.target.style.backgroundColor = "white";
+        }else{
+            event.target.style.backgroundColor = "red";
+        }
+        
+    }
+
+
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
    // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const listItems = numbers.map((number) =>
         <div 
             className="wp-30 hp-30 border-style-solid border-color-android18 border-1 d-flex center-center" 
             id={"sopa_"+number}
+            onClick={handleEvent}
         />
     );
    
