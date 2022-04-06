0e9b8896c11e6f8625796b04a8ec64a0f3194681
clau <claudia.barba@grupo-sm.com>
2022-04-06 12:52:09 -0400

    clau

--- books/ciencias_naturales/3B/reader/src/pages/page026.jsx
+++ books/ciencias_naturales/3B/reader/src/pages/page026.jsx
@@ -79,9 +79,9 @@ function Page026(props) {
           </div>
 
 
-          <div className="d-flex position-relative bgc-red">
+          <div className="d-flex position-relative">
 
-            <div className="d-flex w-100 mtp-0 mbp-0 psp-0 pep-0 ptp-0 pbp-0 position-absolute zindex-10 bgc-green">
+            <div className="d-flex w-100 mtp-0 mbp-0 psp-0 pep-0 ptp-0 pbp-0 position-absolute zindex-10">
               <div className="row m-0 psp-10 pep-10 ptp-10 pbp-0 w-100">
                 <div className="col-12 p-0 m-0 position-relative d-flex center-center">
 
