a9877fc5ff43c0200190f8fb7783c23966f77a0e
Aldo Gonzalez <aldo.gonzalez@colex.grupo-sm.com>
2022-04-08 10:05:48 -0400

    Aldo 8abr

--- books/ciencias_sociales/3B/reader/build/service-worker.js
+++ books/ciencias_sociales/3B/reader/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/libros_abp/books/ciencias_sociales/3B/reader/build/precache-manifest.986f6c4706410a6639770e21325d6068.js"
+  "/libros_abp/books/ciencias_sociales/3B/reader/build/precache-manifest.6c1fb570a2bec6efa0e52ebd63623bb6.js"
 );
 
 workbox.clientsClaim();
