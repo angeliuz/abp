0aa9a883e34f091c4a3bb915d354e3e175e37f98
cristian acevedo <cristian.acevedo@grupo-sm.com>
2022-04-11 13:19:30 -0400

    cristian

--- books/ciencias_sociales/5B/reader/src/pages/page041.jsx
+++ books/ciencias_sociales/5B/reader/src/pages/page041.jsx
@@ -95,7 +95,8 @@ function Page041(props) {
                                             id={"imagen_" + pagina + "_" + (indexInput += 1)}
                                             className="w-100 bgc-white"
                                             colorUnidad={background}
-                                            imagenPlaceHolder="images/generales/subir_imagen.svg"
+                                            imagenPlaceHolder1="images/generales/subir_imagen1.svg"
+                                            imagenPlaceHolder2="images/generales/subir_imagen2.svg"
                                             titulo="Subir imagen"
                                         />
                   </div>
@@ -109,7 +110,8 @@ function Page041(props) {
                                             id={"imagen_" + pagina + "_" + (indexInput += 1)}
                                             className="w-100 bgc-white"
                                             colorUnidad={background}
-                                            imagenPlaceHolder="images/generales/subir_imagen.svg"
+                                            imagenPlaceHolder1="images/generales/subir_imagen1.svg"
+                                            imagenPlaceHolder2="images/generales/subir_imagen2.svg"
                                             titulo="Subir imagen"
                                         />
                   </div>
@@ -123,7 +125,8 @@ function Page041(props) {
                                             id={"imagen_" + pagina + "_" + (indexInput += 1)}
                                             className="w-100 bgc-white"
                                             colorUnidad={background}
-                                            imagenPlaceHolder="images/generales/subir_imagen.svg"
+                                            imagenPlaceHolder1="images/generales/subir_imagen1.svg"
+                                            imagenPlaceHolder2="images/generales/subir_imagen2.svg"
                                             titulo="Subir imagen"
                                         />
                   </div>
@@ -137,7 +140,8 @@ function Page041(props) {
                                             id={"imagen_" + pagina + "_" + (indexInput += 1)}
                                             className="w-100 bgc-white"
                                             colorUnidad={background}
-                                            imagenPlaceHolder="images/generales/subir_imagen.svg"
+                                            imagenPlaceHolder1="images/generales/subir_imagen1.svg"
+                                            imagenPlaceHolder2="images/generales/subir_imagen2.svg"
                                             titulo="Subir imagen"
                                         />
                   </div>
