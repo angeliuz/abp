f24a380e107ca294e866e7748dce6fa91f2aaf80
davidjimenezsm <david.jimenez@grupo-sm.com>
2022-04-12 17:31:13 -0400

    david

--- books/ciencias_naturales/5B/reader/build/service-worker.js
+++ books/ciencias_naturales/5B/reader/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/libros_abp/books/ciencias_naturales/5B/reader/build/precache-manifest.c4e66dd7be72d2675a80e03515624272.js"
+  "/libros_abp/books/ciencias_naturales/5B/reader/build/precache-manifest.57591a284f0a0b3fc8da204559afdec3.js"
 );
 
 workbox.clientsClaim();
