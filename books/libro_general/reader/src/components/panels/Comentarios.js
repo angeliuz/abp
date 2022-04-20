91e47e29b9a6186b7e3a1a9783fe4c569a926399
Jorge Espinoza <jorge.espinoza@grupo-sm.com>
2022-04-20 09:50:36 -0400

    jorge

--- books/libro_general/reader/src/components/panels/Comentarios.js
+++ books/libro_general/reader/src/components/panels/Comentarios.js
@@ -9,8 +9,9 @@ function Comentarios(props) {
     // Visible element react js
 
     const [visible, setVisible] = useState(false);
+    const [responderBoxOpen, setResponderBoxOpen] = useState(false);
 
-    const handleClick = (e) => {
+    const handleClickAgregarComentario = (e) => {
         console.log("hanldeClick", e.target);
         setVisible(!visible);
 
@@ -37,6 +38,30 @@ function Comentarios(props) {
             }, 400); // 500 is the same time as the CSS animation
         }
     }
+
+    const handleClickShowResponder = (e) => {
+        let responderBox = document.querySelector("#responderBox");
+        setResponderBoxOpen(!responderBoxOpen);
+
+        if (responderBoxOpen) {
+            console.log(responderBoxOpen);
+            responderBox.classList.remove("d-flex");
+            responderBox.classList.add("d-none");
+            console.log("Hide responder")
+        } else {
+            console.log(responderBoxOpen);
+            responderBox.classList.remove("d-none");
+            responderBox.classList.add("d-flex");
+            console.log("Show responder")
+        }
+
+    }
+
+    const handleClickResponder = (e) => {
+        console.log("responder")
+    }
+
+
     function removeZeros(pagina) {
         let num = pagina;
         num = num.toString();
@@ -48,7 +73,7 @@ function Comentarios(props) {
         <div>
             <div className="position-absolute btn-agregarComentarios-container top-0 start-100 msp-0 mtp-0 d-flex align-items-start justify-content-end w-auto hp-100">
                 <div className="boton-comentario-container">
-                    <div className="boton-comentario-hijo" onClick={handleClick}>
+                    <div className="boton-comentario-hijo" onClick={handleClickAgregarComentario}>
                         <div className="boton-comentario-nieto">
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24.562 24.562">
                                 <g id="Grupo_55" data-name="Grupo 55" transform="translate(-1009.671 -33)">
@@ -82,7 +107,7 @@ function Comentarios(props) {
                         <div className="w-auto hp-40 d-flex align-items-center flex-grow-1 ">
                             <div className="w-100 psp-5 pep-5 f-Ubuntu-B fsp-18 text-center color-dark">Comentarios</div>
                         </div>
-                        <div className="wp-40 hp-40 mep-10 d-flex align-items-center justify-content-center botonCerrarPanel" onClick={handleClick}>
+                        <div className="wp-40 hp-40 mep-10 d-flex align-items-center justify-content-center botonCerrarPanel" onClick={handleClickAgregarComentario}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="14.477" height="14.477" viewBox="0 0 14.477 14.477">
                                 <path id="Icon_ionic-md-close" data-name="Icon ionic-md-close" d="M22,8.971,20.552,7.523l-5.791,5.791L8.971,7.523,7.523,8.971l5.791,5.791L7.523,20.552,8.971,22l5.791-5.791L20.552,22,22,20.552l-5.791-5.791Z" transform="translate(-7.523 -7.523)" fill="#7e7e7e" />
                             </svg>
@@ -93,7 +118,7 @@ function Comentarios(props) {
                     <div className="flex-grow-1 w-100 h-auto psp-10 pep-10 ptp-0 pbp-10 border-bottom bgc-panelComentarios overflow-scroll">
 
                         <div className="w-100 min-hp-100 h-auto bgc-white rounded-p-10 box-shadow-panels mtp-10">
-                            <div className={"d-flex flex-column justify-content-center psp-10 pep-10 color-white f-Ubuntu-M align-items-start w-100 hp-40 rounded-set-10 " + colorUnidad}>Pagina {removeZeros(pagina)}</div>
+                            <div className={"d-flex flex-column justify-content-center psp-10 pep-10 color-white f-Ubuntu-M align-items-start w-100 hp-40 rounded-set-10  " + colorUnidad}>Pagina {removeZeros(pagina)}</div>
 
                             <div className="w-100 hp-auto d-flex align-items-start psp-10 pep-20 mtp-10 mbp-10">
                                 <div className="wp-35 hp-35">
@@ -118,25 +143,30 @@ function Comentarios(props) {
                             </div>
 
                             <div className="w-100 hp-auto d-flex align-items-start flex-column psp-10 pep-20 mtp--10">
-                                <div className="fsp-11 f-Ubuntu-B color-info w-100 text-end psp-40 mbp-15">Responder</div>
 
-                                <div className="w-100 hp-auto d-flex flex-column align-items-start psp-0 pep-0 mtp-0 mbp-10">
+                                <div className="btn-responder1 fsp-11 f-Ubuntu-B color-info w-100 text-end psp-40 mbp-15 cursor-pointer user-select-none" onClick={handleClickShowResponder}>Responder</div>
+
+                                <div className="w-100 hp-auto d-none flex-column align-items-start psp-0 pep-0 mtp-0 mbp-10 " id="responderBox" >
                                     <div className="w-100 hp-auto d-flex align-items-start psp-0 pep-0 mtp-0 mbp-0">
                                         <div className="wp-35 hp-35">
                                             <div className="w-100 h-100 bgc-color-avatar d-flex center-center rounded-circle f-Ubuntu-B fsp-17 color-avatar">JE</div>
                                         </div>
 
-                                        <div className="wp-150 hp-auto mtp-5 mbp-0 flex-grow-1">
-                                            <div className="w-100 psp-5 pep-0 f-Ubuntu-M fsp-13 text-start mtp--5 "><textarea className="w-100 hp-35 rounded-p-10 border-style-solid border-color-lightDark border-1 p-2 fsp-14"></textarea></div>
+                                        <div className="wp-150 hp-auto mtp-5 mbp-0 mbp-10 flex-grow-1">
+                                            <div className="w-100 psp-5 pep-0 f-Ubuntu-M fsp-13 text-start mtp--5 ">
+                                                {/* <textarea className="w-100 min-hp-35 rounded-p-10 border-style-solid border-color-lightDark border-1 p-2 fsp-14"></textarea> */}
+                                                <div className="w-100 min-hp-35 rounded-p-10 border-style-solid border-color-lightDark border-1 p-2 fsp-14" contentEditable="true" ></div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="w-100 hp-auto d-flex align-items-center justify-content-end psp-0 pep-0 mtp-0 mbp-0">
 
-                                        <div className="w-auto psp-10 pep-10 ptp-5 pbp-5 fsp-13 border-style-solid border-color-lightDark border-1 bgc-white color-info f-Ubuntu-M rounded-p-10 ">Responder</div>
-                                        <div className="w-auto psp-10 pep-10 ptp-5 pbp-5 msp-5 fsp-13 border-style-solid border-color-lightDark border-1 bgc-white color-info f-Ubuntu-M rounded-p-10 ">Cancelar</div>
+                                        <div className="w-auto psp-10 pep-10 ptp-3 pbp-5 fsp-13 border-style-solid border-color-lightDark border-1 bgc-white color-info f-Ubuntu-M rounded-p-10  cursor-pointer user-select-none" onClick={handleClickResponder}>Responder</div>
+                                        <div className="w-auto psp-10 pep-10 ptp-5 pbp-5 msp-5 fsp-13 border-style-solid border-color-lightDark border-1 bgc-white color-info f-Ubuntu-M rounded-p-10  cursor-pointer user-select-none" onClick={handleClickShowResponder}>Cancelar</div>
                                     </div>
 
                                 </div>
+
                             </div>
 
 
